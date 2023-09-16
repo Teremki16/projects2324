bba50f3d551154399dc0f0c548ef67d4aa84f810
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-09-16 09:37:54 +0300

    2

---
+++ SB16 Web Junior/Гординська Еліна/lesson 2/script.js
@@ -0,0 +1,27 @@
+let c = 4
+/*
+console.log("1 + " + a + " = " + (1+a))
+console.log("2 + " + a + " = " + (2+a))
+console.log("3 + " + a + " = " + (3+a))
+console.log("4 + " + a + " = " + (4+a))
+console.log("5 + " + a + " = " + (5+a))
+console.log("6 + " + a + " = " + (6+a))
+console.log("7 + " + a + " = " + (7+a))
+console.log("8 + " + a + " = " + (8+a))
+console.log("9 * " + a + " = " + (9+a))*/
+/*
+console.log(Math.floor(Math.random()*100))*/
+let a = Math.floor(Math.random() * 100)
+
+console.log("Сторона квадрата " + a + " см.")
+
+let S = a * a
+
+console.log(`Площа квадрата зі стороною ${a} см, дорівнює ${S} см2`)
+
+let P = a * 4
+
+console.log("Периметр квадрата зі стороною " + a + " см, дорівнює " + P + " см")
+
+let age = prompt("Скільки тобі років, а?")
+alert(`Ти народився(лася) в ${2023 - age} році`)
