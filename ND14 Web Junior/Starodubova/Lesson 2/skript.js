9da40b1d020d741f47bfbc7cf79f57c1ebad8211
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-09-16 09:12:03 +0300

    2

---
+++ ND14 Web Junior/Starodubova/Lesson 2/skript.js
@@ -0,0 +1,33 @@
+/*
+let number = 5;
+console.log("1 *" + number + "=" + 1 * number);
+console.log("2 *" + number + "=" + 2 * number);
+console.log("3 *" + number + "=" + 3 * number);
+console.log("4 *" + number + "=" + 4 * number);
+console.log("5 *" + number + "=" + 5 * number);
+console.log("6 *" + number + "=" + 6 * number);
+console.log("7 *" + number + "=" + 7 * number);
+console.log("8 *" + number + "=" + 8 * number);
+console.log("9 *" + number + "=" + 9 * number);
+*/
+
+// console.log(Math.round (Math.random()*100))
+
+// let a = (Math.floor (Math.random()*100))
+
+// console.log(`Сторона квадрату дорівнює ${a} см`);
+
+// let S = a * a;
+
+// console.log(`Площа квадрату з стороною ${a} має площу ${S} cм2`)
+
+// let  c = (Math.floor (Math.random()*100))
+
+// let  d = (Math.floor (Math.random()*100))
+
+// console.log(`Перша сторона прямокутника дорівнює ${c} см`)
+// console.log(`Друга сторона прямокутника дорівнює ${d} см`)
+// console.log(`Периметр прямокутника з сторанами ${c} і ${d} дорівнює ${(c+d)*2} см`)
+
+let name = prompt("Як тебе звати?","Вєра")
+alert(`Привіт ${name}, як у тебе справи?`)
---
+++ SB16 Web Junior/гончаренко анастасія/lesson2/script.js
@@ -0,0 +1,14 @@
+let a = Math.floor(Math.random() * 100)
+
+console.log("сторона квадрата" + a + "cм")
+
+let s = a * a
+
+console.log(" площа квадрата з стороною" + a + " cм, дорівнює" + s + " cм2")
+
+
+let age = prompt("скільки тобі років?")
+alert(`ти народився в ${2023 - age} році`)
+
+
+
