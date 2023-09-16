f301ebeb6a034a35c8798f3e4911142b0fcc124f
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-09-16 09:38:44 +0300

    2

---
+++ ND14 Web Junior/Parmenova Valeriia/lesson 1/lesson 2/script.js
@@ -0,0 +1,25 @@
+/*let number = 5
+
+console.log("1 * " + number + "=" + 1 * number)
+console.log("2 * " + number + "=" + 2 * number)
+console.log("3 * " + number + "=" + 3 * number)
+console.log("4 * " + number + "=" + 4 * number)
+console.log("5 * " + number + "=" + 5 * number)
+console.log("6 * " + number + "=" + 6 * number)
+console.log("7 * " + number + "=" + 7 * number) 
+console.log("8 * " + number + "=" + 8 * number)
+console.log("9 * " + number + "=" + 9 * number)*/
+
+/*let a = Math.floor(Math.random() * 100)
+let b = Math.floor(Math.random() * 100)
+
+console.log(`Перша сторона прямокутника орівнює ${a} см`)
+
+console.log(`Друга сторона прямокутника орівнює ${b} см`)
+
+let P = 2*(a+b)
+
+console.log(`Периметр з сторонaми ${a} і ${b} має площу ${P} см`)*/
+
+let name = prompt("Як тебе звати?", "Микита")
+alert(`Привіт ${name}, як у тебе справи?`)
---
+++ SB16 Web Junior/Гординської Ярини/script.js
@@ -0,0 +1,34 @@
+const
+let g = 4
+/*
+console.log("1 + " + a + " = " + (1*a))
+console.log("2 + " + a + " = " + (2*a))
+console.log("3 + " + a + " = " + (3*a))
+console.log("4 + " + a + " = " + (4*a))
+console.log("5 + " + a + " = " + (5*a))
+console.log("6 + " + a + " = " + (6*a))
+console.log("7 + " + a + " = " + (7*a))
+console.log("8 + " + a + " = " + (8*a))
+console.log("9 + "( + a + " = " + (9*a))*/
+/*
+console.log(Math.floor(Math.random()*100 + см))*/
+
+let $a  = Math.floor(Math.random() * 100)
+
+console.log("STORONA KVADRATA " + a + "sm")
+
+let s = 12 / 0
+
+console.log("plosa kvadrata z storonoy " + a + " sm, dorivnye " + s + "sm2")
+
+let p = 4 * a
+
+console.log("plosa " + s + "sm. perimetir dorivnye  " + p + "sm2")
+/*
+let name = prompt(" yak tebe zvati chmo maloletnue?", "chmo")
+alert(`privit, ${name}, ti tupaya mavpa` ); */
+
+let age = prompt("skillki ti vaziz chmo typoe ")
+alert("ti narodivsa v ${2023 - age} chmo typoe haha, idi v lizy")
+
+
