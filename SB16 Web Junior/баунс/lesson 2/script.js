46c579aeb11cb87a2e2556c13603695f95db9242
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-09-16 09:10:12 +0300

    2

---
+++ SB16 Web Junior/баунс/lesson 2/script.js
@@ -0,0 +1,6 @@
+let a = Math.floor(Math.random)()*100
+
+console.log("Сторона квадрата " + a + " см")
+
+let age = promt("how old are u?")
+alert(`ти народився в ${2023 - 14} році`)
---
+++ Ярошенко/lesson 2/script.js
@@ -0,0 +1,38 @@
+/*let number =8
+
+console.log ("1 * "+ number + "=" + 1 * number);
+console.log ("2 * "+ number + "=" + 1 * number);
+console.log ("3 * "+ number + "=" + 1 * number);
+console.log ("4 * "+ number + "=" + 1 * number);
+console.log ("5 * "+ number + "=" + 1 * number);
+console.log ("6 * "+ number + "=" + 1 * number);
+console.log ("7 * "+ number + "=" + 1 * number);
+console.log ("8 * "+ number + "=" + 1 * number);
+console.log ("9 * "+ number + "=" + 1 * number);*/
+
+/*let a = (Math.round(Math.random()*100))
+
+console.log(`Сторона квадрата дорівнює ${a} см`);
+
+let S= a * a 
+console.log(`Площа квадрата зі стороною ${a}  має площу ${S} см2`);*/
+
+
+
+let a = (Math.round(Math.random()*100))
+console.log(`Перша сторона прямокутника дорівнює ${a} см`);
+
+let b = (Math.round(Math.random()*100))
+
+console.log(`Друга сторона прямокутника дорівнює ${b} см`);
+
+let P = (a + b) * 2
+
+console.log(`Периметер прямокутника зі сторонами ${a} та ${b} дорівнює ${P} см2`)
+
+ let name = prompt ("Як тебе звати?", "Людина")
+
+ alert(`Хай ${name}, як у тебе справи?`)
+
+
+
