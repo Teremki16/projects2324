9679797c5ef7c2771da9fe650ce1f5dd02be8a86
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-09-16 09:06:52 +0300

    2

---
+++ ND14 Web Junior/Семчук М/lesson 2/script.js
@@ -0,0 +1,20 @@
+
+/*let number = 774
+
+console.log("1 * " + number + " = " + 1 * number)
+console.log("10 * " + number + " = " + 10 * number)
+console.log("22 * " + number + " = " + 22 * number)
+console.log("98 * " + number + " = " + 98 * number)
+console.log("163 * " + number + " = " + 163 * number)*/
+
+ /*let a = Math.floor(Math.random()*100);
+ let b = Math.floor(Math.random()*100);
+ console.log(`Перша сторона прямокутника дорівнює ${a} см`)
+ console.log(`Друга сторона прямокутника дорівнює ${b} см`)
+
+ let P = 2* (a + b)
+
+ console.log(`Периметр прямокутника з стороною ${P} см2`) */
+
+ let name = prompt("Як тебе звати?", "Микита")
+ alert(`Привіт ${name}, як у тебе справи?`)
