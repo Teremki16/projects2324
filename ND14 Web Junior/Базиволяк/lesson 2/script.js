0bdefaed508e3867b16bfa4d834213812c2ee51d
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-09-16 09:39:37 +0300

    2

---
+++ ND14 Web Junior/Базиволяк/lesson 2/script.js
@@ -0,0 +1,23 @@
+/*
+let  number= 5
+
+console.log ("1 *" + number+"=" +1* number)
+console.log ("2 *" + number+"=" +2* number)
+console.log ("3 *" + number+"=" +3* number)
+console.log ("4 *" + number+"=" +4* number)
+console.log ("5 *" + number+"=" +5* number)
+console.log ("6 *" + number+"=" +6* number)
+console.log ("7 *" + number+"=" +7* number)
+console.log ("8 *" + number+"=" +8* number)
+console.log ("9 *" + number+"=" +9* number)
+*/
+ let a=(Math.round( Math.random()*100 ))
+
+console.log(` перша сторона квадрата дорівнює ${a}см`)
+
+console.log(` друга сторона квадрата дорівнює ${b}см`)
+
+
+let P=2*(a*a)
+
+console.log(`переметер квадрата з стороною ${a} має площу ${S} см2`) 
