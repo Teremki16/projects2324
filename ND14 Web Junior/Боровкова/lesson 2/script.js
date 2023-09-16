70381250c2cefb66b6d23e7679751e6199fb0668
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-09-16 09:37:21 +0300

    2

---
+++ ND14 Web Junior/Боровкова/lesson 2/script.js
@@ -0,0 +1,23 @@
+// let number = 111
+/*
+console.log("1 * " +number + " = "+ 1*number)
+console.log("2 * " +number + " = "+ 2*number)
+console.log("3 * " +number + " = "+ 3*number)
+console.log("4 * " +number + " = "+ 4*number)
+console.log("5 * " +number + " = "+ 5*number)
+console.log("6 * " +number + " = "+ 6*number)
+console.log("7 * " +number + " = "+ 7*number)
+console.log("8 * " +number + " = "+ 8*number)
+console.log("9 * " +number + " = "+ 9*number)
+console.log("10 * " +number + " = "+ 10*number)
+*/
+
+/*
+let a = Math.floor(Math.random() * 100)
+let b = Math.floor(Math.random() * 100)
+console.log(`1 Сторна прямокутнику дорівнює ${a} см< 2 сторона дорівнює ${b}`)
+let P = 2 * a + 2 * b
+console.log(`Периметр прямокутнику дорівнює ${P} см`)
+*/
+let name = prompt ("як тебе звати?", "дашенька<3")
+alert(`привіт ${name}, як справи?`)
---
+++ SB16 Web Junior/Парасочка/script.js
@@ -0,0 +1,19 @@
+let a = Math.random(Math.floor()*100)
+
+console.log("Сторона квадрата " + a + " см")
+
+let s = a * a
+
+console.log("Площа квадрата з стороною " + a + "см, дорівнює " + s + "см2")
+
+let p = a * 4
+
+console.log("Площа квадарата " + p + " см")
+
+let name = prompt("Вам є 18?")
+alert(`Привіт, ${name}, як твоЇ справи?`)
+
+let age = prompt ("Скільки тобі років?")
+alert(`Ти народився в ${2023 - age} році`)
+
+ 
