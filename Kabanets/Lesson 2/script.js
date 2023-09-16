1ed00c966080c660d0a5292cab0b4a943dfaa035
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-09-16 09:08:31 +0300

    2

---
+++ Kabanets/Lesson 2/script.js
@@ -0,0 +1,26 @@
+// let number = 4;
+
+// console.log("1 * " + number + " = " + 1 * number);
+// console.log("2 * " + number + " = " + 2 * number);
+// console.log("3 * " + number + " = " + 3 * number);
+// console.log("4 * " + number + " = " + 4 * number);
+// console.log("5 * " + number + " = " + 5 * number);
+// console.log("6 * " + number + " = " + 6 * number);
+// console.log("7 * " + number + " = " + 7 * number);
+// console.log("8 * " + number + " = " + 8 * number);
+// console.log("9 * " + number + " = " + 9 * number);
+
+// let a = Math.floor( Math.random()*100)
+
+// let b = Math.floor( Math.random()*100)
+
+// console.log(`Перша сторона прямокутника дорівнює ${a} см`);
+
+// console.log(`Друга сторона прямокутника дорівнює ${b} см`)
+
+// let P = 2 * (a + b);
+
+// console.log(`Периметр прямокутника ${P} см`);
+
+let name = prompt("Як справи?", "Анастасія")
+alert(`Привіт ${name}, що робиш?`)
