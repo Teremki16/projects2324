d59ae71b89f214a56ab841eb6e389b1fc927157e
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-09-23 09:13:46 +0300

    3

---
+++ ND14 Web Junior/Семчук М/lesson 3/script.js
@@ -0,0 +1,30 @@
+//let name = prompt("як тебе звати?", "Алєх")
+//let age = +prompt("скільки тобі рочків?" , 67)
+
+//console.log(`Привіт, ${name}`)
+//console.log(`Вітаю в своєму скрипті!`)
+//console.log(`Твій рік народення ${2023 - age}`)
+
+/*let a = Number (prompt("Введіть довжину сторони ребра см"))
+
+console.log(`Куб з ребром ${a} см, має об'єм ${a ** 3} см3 та площу повної поверхні ${6 * (a ** 2)} см2`)*/
+
+/*let age = +prompt("Скільки тобі років?")
+
+if(age < 18) {
+    alert("Шуруй у школу малий гамнюк")
+}else if(age < 21){
+    alert("Шуруй в університет дибіл")
+}else {
+    alert("Шуруй на роботу")
+}*/
+
+/*let password = prompt("enter your password")
+let password1= prompt("enter your password")
+
+if(password == password1) {
+    alert(ura)
+} else {
+    alert("enter you password one more time")
+}*/
+
