bd6535a5af38d1b64efbcfd993b751115d2ed4b5
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-09-23 09:35:49 +0300

    3

---
+++ ND14 Web Junior/Гречишкін Володимир/lesson 1/Lesson 2/Lesson 3/script.js
@@ -0,0 +1,60 @@
+//let name= prompt("Хто ти воїн?","Вася")
+//let age= Number(prompt("Скільки тобі рочків?",6))
+//console.log(`Привіт,${name}`)
+//console.log(`Вітаю в своєму скрипті!`)
+//console.log(`Твій рік народження ${2023-age}`)
+
+//let number = Number(prompt("Введіть число?"))
+
+//console.log(`Ваше число ${number++}`);
+//console.log(`Наступне ${number++}`);
+//console.log(`Наступне ${number}`);
+
+//let number = Number(prompt("Введіть довжину сторони ребра см"))
+
+//console.log(`Куб з ребром ${number} см, має об'єм ${number*number*number} см3 та площу повної поверхні ${6*(number*number)} см2`);
+
+//let age = +prompt("Скільки тобі років?")
+
+//if(age < 18){
+    //alert("Шуруй у школи")
+//}else if(age < 21){
+    //alert ("Шуруй в універ")
+//}else if(age < 39){
+    //alert("Шуруй на роботу")
+//}else{
+    //alert("Шуруй на пенсію")
+//}
+
+let a= +("put number")
+
+if( a%2==0){
+    alert("парне")
+}else{
+    alert ("не парне")
+}
+
+let password=prompt("enter your password")
+let password1=prompt("enter your password")
+
+if(password=password1){
+    alert("all good")
+}else{
+    alert("Ich nicht ferschtein password  enter password one more time")
+}
+
+let b= +prompt("enter your number")
+let b1= +prompt("enter your number")
+let b2= +prompt("enter your number")
+let sum=0
+
+if (a > 0){
+    sum=sum+a
+}
+if (a1 > 0){
+    sum=sum+a1
+}
+if (a2 > 0){
+    sum=sum+a2
+}
+alert(`answer is ${sum}`)
