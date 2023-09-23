b681bc5524da455e160d82e67b97b098b7cff169
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-09-23 09:23:41 +0300

    3

---
+++ ND14 Web Junior/Parmenova Valeriia/lesson 1/lesson 3/script.js
@@ -0,0 +1,42 @@
+/*let name = prompt("Як тебе звати?", "Вася" )
+let age = Number(prompt("Скільки тбі рочків?", 6))
+
+console.log(`Привіт, ${name}`)
+console.log(`Вітаю у свєму скрипті!`)
+console.log(`Твій рік народження ${2023 - age}`)
+
+let number = Number(prompt("Введіть число"))
+
+console.log(`Ваше число ${number++}`)
+console.log(`Наступне ${number++}`)
+console.log(`Наступне ${number}`)*/
+
+/*let a = Number(prompt("Введіть довжину сторони ребра см"))
+
+console.log(`Куб з ребром ${а} см, має об'єм ${а**3} см3 та площу повнї повехні ${6*(a**3)} см2`)*/
+
+/*let age = +prompt("Скільки тобі років?")
+
+if(age < 18){
+    alert("Учі урокі")
+} else if(age < 21) {
+    alert("Шуруй в університет")
+} else if(age < 21) {
+    alert("Шуруй на роботу")
+}*/
+
+/*let a = +prompt("pur the number")
+if (a%2==0){
+    alert("парне число")
+} else{
+    alert("не парне")
+}*/
+
+/*let password = prompt("Введіть пароль")
+let password1 = prompt("Введіть пароль")
+
+if(password==password1){
+     alert("all good")
+} else{
+    alert
+}*/
---
+++ SB16 Web Junior/Гординської Ярини/lesson3/script.js
@@ -0,0 +1,54 @@
+//let Name = prompt("Yak tebe zvati chmo?", "idi v moe vikno");
+//let age = prompt("Skiki tobi rokiv chmo nekrasive?", "bechhh meni 0");
+
+//console.log(`Privit chmo, ${Name}`);
+//console.log("idi v moe vikno chmoo");
+//console.log(`tviu tupie rik narodzenia ${9023 - age}`);
+
+//let number = +prompt("Vedi chislo chmo");
+
+//console.log(`Pershe chislo molodogo chma ${number}`);
+//console.log(`nastypne chislo molodogo chma ${number+1}`);
+//console.log(`nastupne chislo molodogo chma ${number+2}`);
+
+// let a = +prompt("storona kuba? ne snaesh idi v vikno");
+
+// let v = a ** 3 
+
+// let S = 6 * (a ** 2)
+
+// console.log(`storona z rebrom vid KFS ${a} sm mae obeama ${v} ta plosa poverhni chma ${S}`);
+
+
+// let number = +prompt("vvedit chislo")
+
+// console.log(`pershe: ${number % 10}`)
+// console.log(`druuuuuge: ${Math.floor((number % 100)/10)}`)
+// console.log(`Trete nache: ${Math.floor((number % 1000)/100)}`)
+
+// let age = +prompt("skaka tobi roiv")
+
+// if(age <= 18){
+//     alert("pravilo zavzdi nazimai tak, bo tak nikoli ne popadesh na sikavi saiti..")
+// }else if(age <= 21){
+//     alert("FUU MALOLETKA")
+// }
+// else{
+//     alert("tobi menshe 18, idi na raboty")
+// }
+
+// let number = +prompt("vedit chislooooo")
+
+// if(number % 2 == 0){
+//     alert("chislo parne mabut")
+// }else{
+//     alert("chislo nipirni")
+// }
+
+let password = prompt("vidit parol pzppzpzpzpzpzpzppzpz")
+let password1 = prompt("pidtverti mudila")
+if(password1 == password){
+    alert("ptavilno")
+}else{
+    alert("idi")
+}
