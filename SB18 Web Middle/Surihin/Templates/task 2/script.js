4f4590f5c0216c446f789c0d363d4e732c17f449
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-09-23 09:26:45 +0300

    3

---
+++ SB18 Web Middle/Surihin/Templates/task 2/script.js
@@ -0,0 +1,20 @@
+const xhr = new XMLHttpRequest()
+
+const URL = `https://raw.githubusercontent.com/RobocodeSchool/web/master/historycats.json`
+
+xhr.open("GET", URL)
+xhr.responseType = "json"
+xhr.send()
+xhr.onload = () => {
+    let section = document.querySelector("section")
+    section.innerHTML = ""
+    console.log(xhr.response)
+    xhr.response.forEach((i) => {
+        section.innerHTML += `
+        <article>
+            <h2>${i.name}</h2>
+            <img src="${i.img}"></img>
+            <p>${i.description}</p>
+        </article>`
+    })
+}
