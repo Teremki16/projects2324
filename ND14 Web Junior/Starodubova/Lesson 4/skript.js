d532583f70337c0f7edf2ed2842e30b4e1b55a65
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-09-30 09:35:08 +0300

    4

---
+++ ND14 Web Junior/Starodubova/Lesson 4/skript.js
@@ -0,0 +1,46 @@
+// let number = +prompt("Число?")
+// let one = number%10
+// let ten = Math.floor((number%100)/10)
+// let hungred = Math.floor((number%1000)/100)
+// let mil = Math.floor(number/1000)
+
+// if(one +mil==hungred-ten){
+//     alert("Вірно")
+// }else {
+//     alert("Не вірно")
+// }
+
+// let number = +prompt("put your number")
+
+// if(number % 7 == 0 || number % 17 == 0){
+//     alert("This number is great")
+// }else{
+//     alert("This number is not great")
+// }
+
+// let number = +prompt("put your number")
+
+// if((number % 4==0 && number%100!=0) || number % 400 == 0){
+//     alert("Високосний")
+// }else{
+//     alert("Не високосний")
+// }
+
+// let number= +prompt("Скільки разів?")
+// let i=1
+// while(number > 0){
+// console.log(`Випадкове число номер ${i} :${Math.floor(Math.random()*100)}`)
+//     number--
+//     i++
+// }
+
+// let number;
+// let maxNumber=0;
+// do{
+//     number= +prompt("Number?")
+//     if(number>maxNumber){
+//         maxNumber=number
+//     }
+// }while(number==0){
+//     alert("max number was"+ maxNumber)
+// }
---
+++ SB16 Web Junior/гончаренко анастасія/lesson2/lesson4/script.js
@@ -0,0 +1,8 @@
+let num1 = +prompt("put first number")
+let num2 = +prompt("put second number")
+let num3 = +prompt("put third number")
+
+let num4 = (num1 + num2 + num3)/3;
+
+console.log(`year number is ${num4}`)
+
