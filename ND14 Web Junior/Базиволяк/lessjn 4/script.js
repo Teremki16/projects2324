202a40c63ddd5609c61d8a1802821762daf70ab9
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-09-30 09:36:57 +0300

    4

---
+++ ND14 Web Junior/Базиволяк/lessjn 4/script.js
@@ -0,0 +1,46 @@
+//let number = + prompt("put your number")
+
+// let one= number%10
+// let ten= Math.floor ((number%100 )/10)
+// let hund= Math.floor ((number%1000 )/100)
+// let th= Math.floor (number/1000 )
+
+// if (th+one==hund-ten) {
+// alert("yes")
+
+// }else{
+    // alert("no")//
+// }//
+
+// let number = + prompt("Number")
+// if (number%7==0 || number%17){
+    // alert(" число гарне красавчік")
+// }else{
+    // alert(" число не гарне красавчік")
+// }
+
+// let year= +prompt("enter a year")
+// if ( (year%4 ==0 && year%100!=0)|| year%400==0){
+    // alert("високосний")
+// } else {
+    // alert ("не високосний")
+// }
+
+// let count= +prompt("скільки разів згенеувати число?")
+// let i = 1
+// while(count > 0){
+//    console.log(`випадкове число номер ${i} :${Math.floor(Math.random()*100)}`)
+//    count --
+// i++;
+// }
+
+let number;
+let number_max=0
+do{
+ number=+prompt("gg broooooooooooooooooooooooo")
+ if( number > number_max) {
+    number_max=number
+ }
+
+
+}
