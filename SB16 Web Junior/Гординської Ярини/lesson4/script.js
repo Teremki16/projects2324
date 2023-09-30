deb60478c08fd9879090b693812ea5051b218318
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-09-30 09:52:01 +0300

    4

---
+++ SB16 Web Junior/Гординської Ярини/lesson4/script.js
@@ -0,0 +1,69 @@
+// let chislo1 = +prompt("vvedi perse chislo pridurok")
+// console.log(`pershe hislo chma: ${chislo1}`)
+
+// let chislo2 = +prompt("vvedi perse chislo pridurok")
+// console.log(`pershe hislo chma: ${chislo2}`)
+
+// let chislo3 = +prompt("vvedi druge chislo pridurok")
+// console.log(`pershe trete chma: ${chislo3}`)
+
+// let summ = 0;
+
+// if(chislo1 > 0){
+//     summ += chislo1
+// }
+
+// if(chislo2 > 0){
+//     summ += chislo2
+// }
+
+// if(chislo3 > 0){
+//     summ += chislo3
+// }
+
+// console.log(`summa pozitive chisel chma: ${summ}`)
+
+// let number = +prompt("vvedi chislo")
+
+// let one = number % 10;
+
+// let ten = Math.floor ((number % 100) / 10)
+
+// let hund = Math.floor ((number % 1000) / 10)
+
+// let thous = Math.floor (number / 1000)
+
+// if(one + thous == hund + ten){
+//     alert("tak")
+// }else{
+//     alert("tak, haha lol Ni")
+// }
+
+// let number = +prompt("chislo vvidi ppzpzpzpzzppzpzpzpzpzpzpzppzpzpzpzpzpzpzppzpzpzpzpzpzpzpzpzppzpzpzpzpzpzpzpzpzpzpzpzpzpzp")
+
+// if(number % 7 == 0 || number % 17 == 0){
+//     alert(`${number} - waw tvoe chislo super`)
+
+// }else{
+//     alert(`${number} - fuu tvoe chislo chmo`)
+// }
+
+// let year =+prompt("pereviremo rik tvogo chma")
+// if ((year % 4 == 0  && year % 100 != 0)|| year % 400 == 0 ){
+//     alert ("rik norm")
+// }else{
+//     alert("rik figna")
+// }
+
+let num1 = +prompt ("put first number")
+let num2 = +prompt ("put second number")
+let num3 = +prompt ("put third number")
+
+let num4 = (num1 + num2 + num3) / 3
+
+alert(`your number is ${num4}`)
+
+console.log("first number is ${num1}")
+console.log("second number is ${num2}")
+console.log("third number is ${num3}")
+
