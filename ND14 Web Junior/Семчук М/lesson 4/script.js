38b4b37d2733e8f226ccde1de943a6a1054386d4
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-09-30 09:31:33 +0300

    4

---
+++ ND14 Web Junior/Семчук М/lesson 4/script.js
@@ -0,0 +1,39 @@
+/*let number= +prompt("put your number")
+
+let one= number%10
+let ten= Math.floor ((number%100)/10)
+let hund= Math.floor ((number%100)/100)
+let th= Math.floor (number/100)
+
+if (th+one==hund-ten){
+    alert("yes")
+} else{
+    alert("no")
+}
+
+let number = +prompt ("Number?")
+if(number%7 || number%17){
+    alert("Чсило гарне")
+}else{
+    alert("Число гамно")
+}*/
+
+//let year= +prompt("enter a year")
+//if ((year%4 ==0 && year%100!)
+
+/*let count= +prompt("скільки раів згенерувати число?")
+let i = 1;
+while(count > 0){
+    console.log(`випадкове число номер ${1} :${Math.floor(Math.random()*100)}`)
+    count--
+}*/
+
+/*let number;
+let numer_max=0
+do{
+    number==prompt("gdg")
+    if(number>max_number){
+        max_number=number
+    }
+}while(number == 0)
+alert("max number was " + number_max);*/
