8e881a9af88bc964e6489fb900d4e5f5b0d7bc68
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-09-30 09:51:39 +0300

    4

---
+++ ND14 Web Junior/Філімонова/lesson 1/lesson 4/script.js
@@ -0,0 +1,40 @@
+/*let number = +prompt("Напишіть ваше число")
+
+let one = number%10
+let ten = Math.floor((number%100)/10)
+let hund = Math.floor((number%1000)/100)
+let th = Math.floor(number/1000)
+
+if (th+one==hund-ten){
+    alert("yes");
+} else {
+    alert("no");
+}*/
+
+/*let nomber = +prompt("Number?")
+if(number % 7 == 0 ||  number % 17) {
+    alert("Число гарне")
+}else{
+    alert("Число не гарне")
+}*/
+
+/*let year = +prompt("Напишіть рік")
+if (year%4 == 0  year%100!==0 )*/
+
+/*let count = +prompt("Скільки разів згенерувати число?")
+let i = 1
+while(count > 0){
+    console.log(`sdfghjkl ${i} ${(Math.floor(Math.random()*100))}`)
+    count--
+    i++
+}*/
+
+let number;
+let number_max=0
+do{
+    number=+prompt("gbg")
+    if(number>number_max){
+    number_max=number
+    }
+}while(number==0)
+alert("max number was " + number_max);
