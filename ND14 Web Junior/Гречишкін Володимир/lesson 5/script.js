60666526269df813a6557b271ddb20ab7ef860ee
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-10-07 08:53:14 +0300

    5

---
+++ ND14 Web Junior/Гречишкін Володимир/lesson 5/script.js
@@ -0,0 +1,28 @@
+//et number = +prompt ("Put your number")
+//let sum = 0;
+
+//for(let i = number; i > 0 ; i--){
+    //if(number % i == 0) sum += i;
+//}
+//alert("summ is" + sum)
+
+//let number1= +prompt ("Your number 1?")
+//let number2= +prompt ("Your number 2?")
+//let count =0
+
+//for(let i = number1; i <=number2; i++){
+    //let n = (i**3)% 10
+    //if( n ==4 || n==9)count++
+//}
+
+//alert("count"+count)
+
+let m= prompt("Початкова кількість")
+let p= prompt("Відсоток зростання")
+let n= prompt("Кількість днів")
+
+console.log(`Спочатку було${m}  осіб їх відсоток зростання протягом ${n}  днів складає ${p}` )
+for (let i = 1; i<n; i++){
+    m+=m*(p/100)
+    console.log(`День ${i}популяція ${m}`)
+}
---
+++ SB16 Web Junior/Sidlyarenko/lesson_5/script.js
@@ -0,0 +1,39 @@
+// let number = +prompt('Put your number')
+// let i = 1
+// while(number > 0){
+//     let randomNumber = Math.floor(Math.random() * 100)
+//     console.log(`Rabdom number ${i}: ${randomNumber}`)
+//     i++;
+//     number--;
+// }
+
+// let max = 0
+// let n;
+
+// do{
+// n = +prompt('put your number')
+// if(n > max){
+//     max = n
+// }
+// }while(n !=0)
+// alert(`The bigges number is: ${max}`) 
+
+
+// let n = +prompt('put your number')
+// let sum = 0
+
+// for(let i = n; i > 0; i--){
+//     if(n % i ==0){
+//         sum += i
+//     }
+// }
+
+// alert(`sum: ${sum}`)
+
+let a = +prompt(`Введи число 1`)
+let b = +prompt(`Введи число 2`)
+let summ = 0
+for(let i = a; i < b; i++){
+    let t =(i**3) % 10}
+    if(t== 4 || t == 9)
+(`summa vxodyashux chusel`)
