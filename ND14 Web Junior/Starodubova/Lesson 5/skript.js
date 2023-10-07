07120cade0749840935c08de3e6aabc52c35993e
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-10-07 08:47:14 +0300

    5

---
+++ ND14 Web Junior/Starodubova/Lesson 5/skript.js
@@ -0,0 +1,28 @@
+// let number= +prompt("Number?")
+// let sum = 0
+// for(let i = number; i>0; i--){
+// if(number % i == 0)sum += i;
+// }
+// alert("Sum is " + sum)
+
+// let a = +prompt("input a")
+// let b = +prompt("input b")
+// let count = 0
+
+// for(let i = a; i < b; i++){
+//     let t = (i**3) % 10
+//     if(t == 4 || t == 9){
+//         count++
+//     }
+// }
+// alert("count is: " + count)
+
+// let m = +prompt("Стартова кількість?")
+// let p = +prompt("Відсоток?")
+// let n = +prompt("Кількість днів?")
+
+// console.log(`Спочатку було ${m} осіб їх відсоток зростання ${p} протягом ${n} днів`)
+
+// for( let i = 1; i<n; i++){
+//     m+=(m*(p/100))
+//     console.log(`День ${i} популяція ${m}`) 
