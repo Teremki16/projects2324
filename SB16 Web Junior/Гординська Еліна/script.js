2b848ee8929b885269a009379653eb88e5409181
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-10-21 08:58:27 +0300

    8

---
+++ SB16 Web Junior/Гординська Еліна/script.js
@@ -0,0 +1,44 @@
+// let size = +prompt("How much numbers do you want/need?")
+
+// let arr = [];
+
+// for(let i = 0; i < size; i++){
+//     arr.push(Math.floor(Math.random()*100));
+// }
+
+// console.log(arr)
+
+// function summArray(array){
+//     let sum = 0;
+//     for(let number of array){
+//         sum += number
+//     }
+//     return sum
+// }
+
+// console.log(`Summ of elements: ${summArray(arr)}`)
+
+
+// function showArrayElem(array){
+//     for(let i = 0; i < array.length; i++){
+//         console.log(`arr[${i}] = ${array[i]}`)
+//     }
+// }
+
+// showArrayElem(arr)
+
+
+let elem;
+let arr = []
+
+
+do{
+    elem = +prompt("Iput your number")
+    if(arr.includes(elem)){
+        continue
+    }else {
+        arr.push(elem)
+    }
+}while(elem)
+
+console.log(`array without dublicates: ${arr}`)
