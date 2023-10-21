d9921bf7254e2381234517452aec1f34e78b71bc
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-10-21 09:21:40 +0300

    8

---
+++ Ярошенко/lesson 7/script.js
@@ -0,0 +1,36 @@
+let arr = []
+let count = +prompt("How many rundom numbers do you want")
+for(let i = 0; i < count; i++){
+    arr.push(Math.floor(Math.random()*100));
+}
+
+console.error(arr);
+
+function arraySumm(array){
+    let summ = 0;
+    for(let i = 0; i < array.length; i++){
+        summ += array[i];
+    }
+    return summ;
+}
+console.log(`summ of elements of array ${arraySumm(arr)}`)
+
+function showArray (array){
+    for(let i = 0; i < array.length; i++){
+        console.log(`arr[${i}]= ${array[i]}`)
+    }
+}
+showArray(arr);
+
+let array = []
+let elem;
+do{
+    elem = +prompt("imput random number")
+    if(elem == 0) break
+    if(array.includes(elem)){
+        continue
+    }else{
+        array.push(elem)
+    } 
+}while(elem);
+console.log (`Array without dublicates:`,array)
