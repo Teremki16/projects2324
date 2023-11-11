fdd375fd3167f188a8f2aac40ac82cd24495088a
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-11-11 09:43:18 +0200

    11

---
+++ ND14 Web Junior/Філімонова/lesson 9/script.js
@@ -0,0 +1,30 @@
+//console.dir(document.body)
+
+//console.log(document.querySelector("h1").innerHTML)
+
+//document.querySelector("h1").innerHTML = "<u>Hello Mykyta</u>"
+
+//setInterval(()=>{
+//let elem = document.querySelector("h1")
+//elem.hidden = !elem.hidden
+//}, 500)
+
+//const changeBG = ()=>{
+  //  let val = document.querySelector("input").value
+  //  document.body.style.background = val
+//}
+
+let count = 1;
+
+function change(){
+  let value = document.querySelector("input").value
+  document.querySelector(".result").innerHTML += `${count}. ${value} <br>`
+  count++;
+  document.querySelector("input").value = "";
+
+}
+
+function clearNote(){
+  document.querySelector(".result").innerHTML = ""
+  count = 1;
+} 
