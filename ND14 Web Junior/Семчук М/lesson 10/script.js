b9c0e0fd4fb66ac129b018d67fec0783e5f5aa79
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-11-11 10:54:17 +0300

    11

---
+++ ND14 Web Junior/Семчук М/lesson 10/script.js
@@ -0,0 +1,29 @@
+//function CC(){
+//    let links = document.querySelectorAll("a")
+//    for(let link of links){
+//        let href = link.getAttribute("href")
+//        if(!href) continue;
+//        if(href.includes("://")) link.style.color = "pink"
+//    }
+//}
+//function RC(){
+//    let links = document.querySelectorAll("a")
+//    for(let link of links){
+//        link.style.color = "black"
+//    }
+//} 
+
+let count = 1;
+let ul = document.querySelector(".todolist")
+
+function add(){
+    let value = document.querySelector("#text").value
+    if(value){
+        let elem = document.createElement("li")
+        elem.innerText = value;
+        elem.id = `Item${count}`
+        count++;
+        ul.append(elem)
+        document.querySelector("#text").value = ""
+    }
+}
