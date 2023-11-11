7953ae76dde1c140f2d203a2f803e95d72eac70d
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-11-11 09:56:50 +0200

    11

---
+++ ND14 Web Junior/лисач/lesson10/script.js
@@ -0,0 +1,17 @@
+function CC(){
+    let links = document.querySelectorAll("a")
+    for(let link of links){
+        let href = link.getAttribute("href")
+        if(!href) continue;
+        if(href.includes("://")) link.style.color = "red"
+
+    }
+}
+
+
+function RC(){
+    let links = document.querySelectorAll("a")
+    for(let link of links){
+        link.style.color = "black"
+    }
+}
--- ND14 Web Junior/лисач/lesson9/script.js
+++ ND14 Web Junior/лисач/lesson9/script.js
@@ -1,16 +1,33 @@
-// console.dir(document.body)
+// // console.dir(document.body)
 
-// console.log(document.querySelector("h1").innerHTML)
+// // console.log(document.querySelector("h1").innerHTML)
 
-// document.querySelector("h1").innerHTML = "<u>Hello MyKyta</u>"
+// // document.querySelector("h1").innerHTML = "<u>Hello MyKyta</u>"
 
 
-// setInterval(()=>{
-//     let elem = document.querySelector("h1")
-//     elem.hidden = !elem.hidden
-// }, 500)
+// // setInterval(()=>{
+// //     let elem = document.querySelector("h1")
+// //     elem.hidden = !elem.hidden
+// // }, 500)
 
-const changeBG = ()=>{
-    let val = document.querySelector("input").value
-    document.body.style.background = val
+// const changeBG = ()=>{
+//     let val = document.querySelector("input").value
+//     document.body.style.background = val
+// }
+
+
+let count = 1;
+function change(){
+    let value = document.querySelector("input").value
+    document.querySelector(".result").innerHTML+= `${count}.${value} <br>` 
+    count++
+    document.querySelector("input").value ="";
+
+
+}
+
+
+function clearNote(){
+    document.querySelector(".result").innerHTML = ""
+    count = 1;
 }
