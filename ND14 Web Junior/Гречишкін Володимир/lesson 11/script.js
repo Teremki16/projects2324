611e24e3726f091109061a6ef7c6d7cba3de5e62
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-11-26 09:50:41 +0300

    13

--- ND14 Web Junior/Гречишкін Володимир/lesson 11/script.js
+++ ND14 Web Junior/Гречишкін Володимир/lesson 11/script.js
@@ -1,29 +1,43 @@
 let canvases = document.querySelectorAll("canvas")
 let ctx = []
 
-for(let i = 0;i < canvases.length;i++){
+for (let i = 0; i < canvases.length; i++) {
     canvases[i].width = 700;
     canvases[i].height = 700;
     ctx.push(canvases[i].getContext("2d"))
 }
 
-ctx[0].moveTo(270,200)
-ctx[0].lineTo(350,300)
-ctx[0].lineTo(450,150)
-ctx[0].lineWidth=5;
+ctx[0].moveTo(270, 200)
+ctx[0].lineTo(350, 300)
+ctx[0].lineTo(450, 150)
+ctx[0].lineWidth = 5;
 ctx[0].stroke()
 
 ctx[1].beginPath()
-ctx[1].arc(350,250,100,(20*Math.PI)/180,(330*Math.PI)/180,)
-ctx[1].lineTo(350,250);
-ctx[1].lineWidth=5
+ctx[1].arc(350, 250, 100, (20 * Math.PI) / 180, (330 * Math.PI) / 180,)
+ctx[1].lineTo(350, 250);
+ctx[1].lineWidth = 5
 ctx[1].closePath()
 ctx[1].stroke()
-ctx[1].fillStyle="yellow"
+ctx[1].fillStyle = "yellow"
 ctx[1].fill()
 
 ctx[1].beginPath()
-ctx[1].fillStyle="black"
-ctx[1].arc(350,210,5,(10*Math.PI)/180,(360*Math.PI)/180)
+ctx[1].fillStyle = "black"
+ctx[1].arc(350, 210, 5, (10 * Math.PI) / 180, (360 * Math.PI) / 180)
 ctx[1].closePath()
 ctx[1].fill()
+
+ctx[2].strokeRect(215, 115, 266, 266)
+ctx[2].strokeRect(218, 118, 260, 260)
+ctx[2].fillStyle = "brown";
+ctx[2].fillRect(220, 120, 256, 256);
+
+
+ctx[2].fillStyle = "black"
+for (let i = 0; i < 8; i += 2) {
+    for (let j = 0; j < 8; j += 2) {
+        ctx[2].fillRect(220 + i * 32, 120 + j * 32, 32, 32)
+        ctx[2].fillRect(252 + i * 32, 152 + j * 32, 32, 32)
+    }
+}
--- SB16 Web Junior/Sidlyarenko/lesson_11/script.js
+++ SB16 Web Junior/Sidlyarenko/lesson_11/script.js
@@ -6,17 +6,58 @@ let renge = document.getElementById("range")
 let rangeText = document.getElementById("rangeValue")
 
 let lw = 10
-let color = "black"
+let color = "red"
 let isMouseDown = false
 
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 function fillin(){
-    ctx.fillStyle = "black"
+    ctx.fillStyle = color
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 }
 
 function clearAll(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 }
+
+colorPicker.addEventListener("change", (event)=>{
+    color = event.target.value
+})
+
+canvas.addEventListener("mousedown", ()=>{
+    isMouseDown = true
+    ctx.beginPath()
+})
+
+canvas.addEventListener("mouseup", ()=>{
+    isMouseDown = false
+})
+
+canvas.addEventListener("mousemove", (event)=>{
+    if(isMouseDown){
+        ctx.fillStyle = color
+        ctx.strokeStyle = color
+        ctx.lineWidth = lw
+
+        ctx.lineTo(event.clientX, event.clientY)
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.arc(event.clientX, event.clientY, lw / 2, 0, 2 * Math.PI)
+        ctx.fill()
+
+        ctx.beginPath()
+        ctx.moveTo(event.clientX, event.clientY)
+    }
+})
+
+range.addEventListener("input", (event)=>{
+    lw = event.target.value
+    rangeText.innerText = lw
+})
+
+document.querySelector("a").addEventListener("click", ()=>{
+    let url = canvas.toDataURL().replace("image/png", "image/octet-stream")
+    document.querySelector("a").href = url
+})
