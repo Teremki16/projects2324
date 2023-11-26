a765f837ae02659495aabf75e731caa205c5f796
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-11-26 08:48:29 +0200

    13

--- SB16 Web Junior/Гординської Ярини/lesson12/script.js
+++ SB16 Web Junior/Гординської Ярини/lesson12/script.js
@@ -13,7 +13,7 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 function fillin(){
-    ctx.fillStyle = "black"
+    ctx.fillStyle = color
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 }
 
@@ -21,3 +21,44 @@ function clearAll(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
 }
+
+colorPicker.addEventListener("change", (event)=>{
+    color = event.target.value
+});
+
+canvas.addEventListener("mousedown", ()=>{
+    isMouseDown = true;
+    ctx.beginPath();
+});
+
+canvas.addEventListener("mouseup", ()=>{
+    isMouseDown = false;
+});
+
+canvas.addEventListener("mousemove", (event)=>{
+    if(isMouseDown){
+        ctx.fillStyle = color
+        ctx.strokeStyle = color
+        ctx.lineWidth = lw
+
+        ctx.lineTo(event.clientX, event.clientY)
+        ctx.stroke()
+
+        ctx.beginPath();
+        ctx.arc(event.clientX, event.clientY, lw / 2, 0, 2* Math.PI)
+        ctx.fill()
+
+        ctx.beginPath();
+        ctx.moveTo(event.clientX, event.clientY)
+    }
+});
+
+range.addEventListener("input", (event)=>{
+    lw = event.target.value
+    rangeText.innetText = lw
+});
+
+document.querySelector("a").addEventListener("click", (event)=>{
+    let url = canvas.toDataURL().replace("image/png", "image/octet-stream")
+    document.querySelector("a").href = url
+});
