7a0ed95e9fcd5907378e58c00aa6fe3b2ade9545
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-11-26 08:48:01 +0200

    13

--- ND14 Web Junior/Базиволяк/lesson11/script.js
+++ ND14 Web Junior/Базиволяк/lesson11/script.js
@@ -34,3 +34,16 @@ ctx[0].lineTo(190, 20)
 ctx[0].stroke ()
 ctx[1].beginPath();
 ctx
+
+ctx[2].strokeRect (215,115,266,266)
+ctx[2].strokeRect (218,118,260,260)
+ctx[2].fillStyle = "brown"
+ctx[2].fillRect(220,120,256,256)
+ctx[2].fillStyle = "black"
+
+for(let i = 0; i < 8; i+=2){
+    for(let j = 0; j < 8; j+=2){
+ ctx[2].fillRect(220 + i * 32, 120 + j * 32 , 32 ,32)
+ ctx[2].fillRect(252 + i * 32, 152 + j * 32 , 32 ,32)
+    }
+}
