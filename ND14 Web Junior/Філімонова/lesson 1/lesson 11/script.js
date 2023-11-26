4411cb6bc7cbeedd5a6b664f598f673dec7b7f5c
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-11-26 08:49:13 +0200

    13

--- ND14 Web Junior/Філімонова/lesson 1/lesson 11/script.js
+++ ND14 Web Junior/Філімонова/lesson 1/lesson 11/script.js
@@ -28,4 +28,17 @@ ctx[1].fillStyle = "black"
 ctx[1].arc(380, 190, 10, 0, 20* Math.PI)
 ctx[1].closePath()
 ctx[1].fill()
-ctx[1].lineWidth = 10;
+
+ctx[2].strokeRect(215, 115, 266,266)
+ctx[2].strokeRect(218, 118, 260,260)
+ctx[2].fillStyle = "brown"
+ctx[2].fillRect(220, 120, 256, 256);
+
+ctx[2].fillStyle = "black";
+
+for(let i = 0; i < 8; i+=2){
+    for(let j = 0; j < 8; j += 2){
+        ctx[2].fillRect(220 + i * 32, 120 + j * 32, 32, 32)
+        ctx[2].fillRect(252 + i * 32, 152 + j * 32, 32, 32)
+    }
+}
--- SB16 Web Junior/Гординська Еліна/lesson 12/script.js
+++ SB16 Web Junior/Гординська Еліна/lesson 12/script.js
@@ -13,10 +13,51 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 function fillin(){
-    ctx.fillStyle = "orange"
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = color
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function clearAll(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+colorPicker.addEventListener("change", (event)=>{
+    color = event.target.value;
+});
+
+canvas.addEventListener("mousedown", ()=>{
+    isMouseDown = true;
+    ctx.beginPath();
+});
+
+canvas.addEventListener("mouseup", ()=>{
+    isMouseDown = false;
+});
+
+canvas.addEventListener("mousemove", (event)=>{
+    if(isMouseDown){
+        ctx.fillStyle = color
+        ctx.strokeStyle = color
+        ctx.lineWidth = lw
+
+        ctx.lineTo(event.clientX, event.clientY)
+        ctx.stroke()
+
+        ctx.beginPath();
+        ctx.arc(event.clientX, event.clientY, lw / 2, 0, 2 * Math.PI)
+        ctx.fill()
+
+        ctx.beginPath()
+        ctx.moveTo(event.clientX, event.clientY);
+    }
+})
+
+range.addEventListener("input", (event)=>{
+    lw = event.target.value
+    rangeText.innerText = lw
+})
+
+document.querySelector("a").addEventListener("click", (event)=>{
+    let url = canvas.toDataURL().replace("image/png", "image/octet-stream")
+    document.querySelector("a").href = url
+})
