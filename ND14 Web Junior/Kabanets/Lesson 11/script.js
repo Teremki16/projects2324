c77aef7cc486efa93e7f51c81068957c6a289fdc
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-11-26 08:43:04 +0200

    13

--- ND14 Web Junior/Kabanets/Lesson 11/script.js
+++ ND14 Web Junior/Kabanets/Lesson 11/script.js
@@ -26,3 +26,17 @@ ctx[1].beginPath()
 ctx[1].fillStyle = "black"
 ctx[1].arc(360, 210, 10, (20 * Math.PI) /180,(350 * Math.PI) /180);
 ctx[1].fill()
+
+ctx[2].strokeRect(215, 115, 266, 266)
+ctx[2].strokeRect(218, 118, 260, 260)
+ctx[2].fillStyle = "brown"
+ctx[2].fillRect(220, 120,256, 256)
+
+ctx[2].fillStyle = "black"
+
+for(let i = 0; i < 8; i+=2){
+   for(let j = 0; j < 8; j += 2){
+       ctx[2].fillRect(220 + i * 32, 120 + j * 32, 32, 32)
+       ctx[2].fillRect(252 + i * 32, 152 + j * 32, 32, 32)
+   }
+}
