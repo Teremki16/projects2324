f4b7a9e709228535eac475d83736c1a07830fff1
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2023-12-03 08:42:34 +0200

    14

--- SB14 Middle embedded/Zakharov/Zakharov.ino
+++ SB14 Middle embedded/Zakharov/Zakharov.ino
@@ -2,8 +2,8 @@
 SoftwareSerial bt(2, 3);
 int carSpeed = 150;
 char value;
-bt.begin(9600);
 void setup() {
+  bt.begin(9600);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
@@ -17,139 +17,141 @@ void setup() {
 
 void loop() {
   getBluetoothMessage();
-switch (value);
-  case'F'
-forward (carSpeed);
-break;
-  case'B'
-backward (carSpeed);
-break;
-    case'L'
-left (carSpeed);
-break;
-  case'R'
-right (carSpeed);
-break;
-  case'S'
-stp ();
-break;
-  case'G'
-forwardLeft();
-break;
-  case'I'
-forwardRight();
-break;
-  case'J'
-backwardRight();
-break;
-  case'H'
-backwardLeft();
-break;
-  case'1'
-carSpeed - 70;
-break;
-  case'2'
-carSpeed - 90;
-break;
-  case'3'
-carSpeed - 110;
-break;
-  case'4'
-carSpeed - 130;
-break;
-  case'5'
-carSpeed - 150;
-break;
-  case'6'
-carSpeed - 170;
-break;
-  case'7'
-carSpeed - 190;
-break;
-  case'8'
-carSpeed - 210;
-break;
-  case'9'
-carSpeed - 230;
-break;
-  case'q'
-carSpeed - 255;
-break;
+  switch (value) {
+    case'F':
+      forward (carSpeed);
+      break;
+    case'B':
+      backward (carSpeed);
+      break;
+    case'L':
+      left (carSpeed);
+      break;
+    case'R':
+      right (carSpeed);
+      break;
+    case'S':
+      stp ();
+      break;
+    case'G':
+      forwardLeft();
+      break;
+    case'I':
+      forwardRight();
+      break;
+    case'J':
+      backwardRight();
+      break;
+    case'H':
+      backwardLeft();
+      break;
+    case'1':
+      carSpeed = 70;
+      break;
+    case'2':
+      carSpeed = 90;
+      break;
+    case'3':
+      carSpeed = 110;
+      break;
+    case'4':
+      carSpeed = 130;
+      break;
+    case'5':
+      carSpeed = 150;
+      break;
+    case'6':
+      carSpeed = 170;
+      break;
+    case'7':
+      carSpeed = 190;
+      break;
+    case'8':
+      carSpeed = 210;
+      break;
+    case'9':
+      carSpeed = 230;
+      break;
+    case'q':
+      carSpeed = 255;
+      break;
+  }
 }
-void forward (int sped) {
-  analogWrite(5, sped);
-  digitalWrite(6, LOW);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, HIGH);
-  digitalWrite(9, LOW);
-  analogWrite(10, sped);
-}
-void backward (int sped) {
-  analogWrite(5, sped);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, LOW);
-  digitalWrite(8, LOW);
-  digitalWrite(9, HIGH);
-  analogWrite(10, sped);
-}
-void right (int sped) {
-  analogWrite(5, sped);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, LOW);
-  digitalWrite(8, HIGH);
-  digitalWrite(9, LOW);
-  analogWrite(10, sped);
-}
-void left (int sped) {
-  analogWrite(5, sped);
-  digitalWrite(6, LOW);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, LOW);
-  digitalWrite(9, HIGH);
-  analogWrite(10, sped);
-}
-void stp () {
-  analogWrite(5, sped);
-  digitalWrite(6, LOW);
-  digitalWrite(7, LOW);
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
-  analogWrite(10, sped);
-}
-void forwardLeft(int sped) {
-  analogWrite(5, sped);
-  digitalWrite(6, LOW);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
-  analogWrite(10, sped);
-}
-void forwardRight(int sped) {
-  analogWrite(5, sped);
-  digitalWrite(6, LOW);
-  digitalWrite(7, LOW);
-  digitalWrite(8, HIGH);
-  digitalWrite(9, LOW);
-  analogWrite(10, sped);
-}
-void backwardLeft(int sped) {
-  analogWrite(5, sped);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, LOW);
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
-  analogWrite(10, sped);
-}
-void backwardRight(int sped) {
-  analogWrite(5, sped);
-  digitalWrite(6, LOW);
-  digitalWrite(7, LOW);
-  digitalWrite(8, LOW);
-  digitalWrite(9, HIGH);
-  analogWrite(10, sped);
-}
-void getBluetoothMessage
-if (bt.avalibe)) {
-value = bt.read();
-Serial.println("Message from phone: " + (String)value);
-}
+  void forward (int sped) {
+    analogWrite(5, sped);
+    digitalWrite(6, LOW);
+    digitalWrite(7, HIGH);
+    digitalWrite(8, HIGH);
+    digitalWrite(9, LOW);
+    analogWrite(10, sped);
+  }
+  void backward (int sped) {
+    analogWrite(5, sped);
+    digitalWrite(6, HIGH);
+    digitalWrite(7, LOW);
+    digitalWrite(8, LOW);
+    digitalWrite(9, HIGH);
+    analogWrite(10, sped);
+  }
+  void right (int sped) {
+    analogWrite(5, sped);
+    digitalWrite(6, HIGH);
+    digitalWrite(7, LOW);
+    digitalWrite(8, HIGH);
+    digitalWrite(9, LOW);
+    analogWrite(10, sped);
+  }
+  void left (int sped) {
+    analogWrite(5, sped);
+    digitalWrite(6, LOW);
+    digitalWrite(7, HIGH);
+    digitalWrite(8, LOW);
+    digitalWrite(9, HIGH);
+    analogWrite(10, sped);
+  }
+  void stp () {
+    analogWrite(5, sped);
+    digitalWrite(6, LOW);
+    digitalWrite(7, LOW);
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+    analogWrite(10, sped);
+  }
+  void forwardLeft(int sped) {
+    analogWrite(5, sped);
+    digitalWrite(6, LOW);
+    digitalWrite(7, HIGH);
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+    analogWrite(10, sped);
+  }
+  void forwardRight(int sped) {
+    analogWrite(5, sped);
+    digitalWrite(6, LOW);
+    digitalWrite(7, LOW);
+    digitalWrite(8, HIGH);
+    digitalWrite(9, LOW);
+    analogWrite(10, sped);
+  }
+  void backwardLeft(int sped) {
+    analogWrite(5, sped);
+    digitalWrite(6, HIGH);
+    digitalWrite(7, LOW);
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+    analogWrite(10, sped);
+  }
+  void backwardRight(int sped) {
+    analogWrite(5, sped);
+    digitalWrite(6, LOW);
+    digitalWrite(7, LOW);
+    digitalWrite(8, LOW);
+    digitalWrite(9, HIGH);
+    analogWrite(10, sped);
+  }
+  void getBluetoothMessage() {
+    if (bt.avalibe)) {
+      value = bt.read();
+      Serial.println("Message from phone: " + (String)value);
+    }
+  }
--- SB16 Web Junior/AkimovaYuliaWeb/Flappy4JS/flappy.js
+++ SB16 Web Junior/AkimovaYuliaWeb/Flappy4JS/flappy.js
@@ -25,9 +25,45 @@ let Ypos = 150
 let g=0.2
 let velY=0
 
+let gap=100
+
+let pipe=[]
+pipe[0]={
+    x:canvas.width,
+    y:0,
+}
+
 function draw(){
     ctx.drawImage(back,0,0)
     ctx.drawImage(bird,Xpos,Ypos)
+    velY+=g
+    Ypos+=velY;
+    if(Ypos+ bird.height>canvas.height-road.height){
+        location.reload()
+    }
+    for(let i = 0;i < pipe.length;i++){
+        ctx.drawImage(pipeUp,pipe[i].x,pipe[i].y)
+        ctx.drawImage(pipeBottom,pipe[i].x,pipe[i].y+pipeUp.height+gap)
+        pipe[i].x-=2
+
+        if(pipe[i].x==90){
+            pipe.push({
+                x:canvas.width,
+                y:Math.floor(Math.random()*pipeUp.height)-pipeUp.height
+            })
+        }
+    }
+    ctx.drawImage(road,0, canvas.height-road.height)
 }
 
 setInterval(draw,20)
+
+function moveUp(){
+    if(Ypos>=0){
+        velY=-5;
+        flyAudio.play()
+    }
+}
+
+canvas.addEventListener("click",moveUp)
+
