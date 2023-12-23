2f392d7c0d63debc2c703897ca59ce08cb53d735
Teremki16 <teremki16robocode@gmail.com>
2023-12-23 09:24:53 +0300

    Update at 23.12.2023

---
+++ ND14 Web Junior/Гречишкін Володимир/lesson 17/script.js
@@ -0,0 +1,35 @@
+let start = $.now()
+
+$(document).ready(()=>{
+    // alert(`Tite to ready:${$.now() - start}`)
+})    
+$("h1").click(()=>{
+    alert("This is heading")
+})
+$("p").click(()=>{
+    alert("This is paragraph")
+})
+$("a").click(()=>{
+    alert("This is link")
+})
+$("h1,p,a").click((event)=>{
+    event.preventDefault()
+    let elem=event.target
+    console.log("User clicked on:", elem.tagName)
+})
+
+$(".show").click(()=>{
+    $(".text").show()
+})
+
+$(".hide").click(()=>{
+    $(".text").hide()
+})
+
+$(".a").click(function(){
+    $(this).hide()
+});
+
+$(".showAll").click(function(){
+    $(".a").show()
+})
