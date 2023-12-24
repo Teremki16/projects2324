d5bd06d58856d008d1fdb18df89724bde2cedb9e
Teremki16 <teremki16robocode@gmail.com>
2023-12-24 08:33:16 +0200

    Update at 24.12.2023

--- SB14 Middle embedded/Vitovetskyi_Maxim1/Vitovetskyi_Maxim1.ino
+++ SB14 Middle embedded/Vitovetskyi_Maxim1/Vitovetskyi_Maxim1.ino
@@ -1,7 +1,8 @@
 #include <SoftwareSerial.h>
-SoftwareSerial bt (2, 3);//RX = 2, TX = 3
-int carSpeed = 150;
+SoftwareSerial bt(2, 3); //RX = 2, TX = 3
+int carSpeed = 255;
 char value;
+bool lineMode = false;
 void setup() {
   bt.begin(9600);
   pinMode(5, OUTPUT);
@@ -14,17 +15,19 @@ void setup() {
   pinMode(12, INPUT);
   Serial.begin(9600);
 }
+
 void loop() {
-  Move();
+   Move();
 }
+
 void checkLineSensors() {
-  Serial.print("Left: " + (String)digitalRead(11) + " ");
+  Serial.println("Left: " + (String)digitalRead(11));
   Serial.println("Right: " + (String)digitalRead(12));
   delay(500);
 }
 
 void Move() {
-  getBluetootMessage();
+  getBluetoothMessage();
   switch (value) {
     case 'F':
       forward(carSpeed);
@@ -47,26 +50,60 @@ void Move() {
       break;
 
     case 'G':
-      forwardleft(carSpeed);
+      forwardLeft(carSpeed);
       break;
 
     case 'I':
-      forwardright(carSpeed);
+      forwardRight(carSpeed);
       break;
 
     case 'H':
-      Backwardleft(carSpeed);
+      backwardLeft(carSpeed);
       break;
 
     case 'J':
-      backwardright(carSpeed);
+      backwardRight(carSpeed);
       break;
 
+    case '1':
+      carSpeed = 70;
+      break;
 
+    case '2':
+      carSpeed = 90;
+      break;
 
+    case '3':
+      carSpeed = 110;
+      break;
 
+    case '4':
+      carSpeed = 130;
+      break;
 
+    case '5':
+      carSpeed = 150;
+      break;
 
+    case '6':
+      carSpeed = 170;
+      break;
+
+    case '7':
+      carSpeed = 190;
+      break;
+
+    case '8':
+      carSpeed = 210;
+      break;
+
+    case '9':
+      carSpeed = 230;
+      break;
+
+    case 'q':
+      carSpeed = 255;
+      break;
   }
 
 }
@@ -76,7 +113,7 @@ void forward(int sped) {
   digitalWrite(7, HIGH);
   digitalWrite(8, HIGH);
   digitalWrite(9, LOW);
-  digitalWrite(10, sped);
+  analogWrite(10, sped);
 
 }
 void backward(int sped) {
@@ -95,7 +132,7 @@ void left(int sped) {
   digitalWrite(9, HIGH);
   analogWrite(10, sped);
 }
-void righ(int sped) {
+void right(int sped) {
   analogWrite(5, sped);
   digitalWrite(6, HIGH);
   digitalWrite(7, LOW);
@@ -111,16 +148,15 @@ void stp() {
   digitalWrite(9, LOW);
   analogWrite(10, 0);
 }
-void forwardLeft() {
+void forwardLeft(int sped) {
   analogWrite(5, sped);
   digitalWrite(6, LOW);
   digitalWrite(7, HIGH);
   digitalWrite(8, LOW);
   digitalWrite(9, LOW);
   analogWrite(10, sped);
-
 }
-void forwardRigh() {
+void forwardRight(int sped) {
   analogWrite(5, sped);
   digitalWrite(6, LOW);
   digitalWrite(7, LOW);
@@ -128,9 +164,26 @@ void forwardRigh() {
   digitalWrite(9, LOW);
   analogWrite(10, sped);
 }
-void getBluetootMessage() {
+void backwardLeft(int sped) {
+  analogWrite(5, sped);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  analogWrite(10, sped);
+}
+void backwardRight(int sped) {
+  analogWrite(5, sped);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  analogWrite(10, sped);
+}
+void getBluetoothMessage() {
   if (bt.available()) {
     value = bt.read();
-    Serial.println("Message from phone: " + (String)value;
+    Serial.println("Message from phone: " + (String)value);
   }
+
 }
