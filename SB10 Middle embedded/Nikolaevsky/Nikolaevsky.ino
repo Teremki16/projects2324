e9d5a9e7f78c773560a2e58e3523165d939b2dfd
Teremki16 <teremki16robocode@gmail.com>
2023-12-24 08:30:07 +0200

    Update at 24.12.2023

--- SB10 Middle embedded/Nikolaevsky/Nikolaevsky.ino
+++ SB10 Middle embedded/Nikolaevsky/Nikolaevsky.ino
@@ -1,56 +1,28 @@
-#include <SoftwareSerial.h>
-SoftwareSerial bt(2, 3)
+#include<SoftwareSerial.h>
+SoftwareSerial bt(2, 3);  //DRX-2, DTX-3
 
-chat value
+char value;
 void setup() {
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
   pinMode(11, INPUT);
   pinMode(12, INPUT);
   Serial.begin(9600);
   bt.begin(9600);
 }
 
-void loop(){
-  Move;
-  checkBluetooth
-  
-/*forward(225);
-  delay(1000)
-  stp();}
-  delay(1000);}
-
-  backward(225);
-  delay(1000)
-  stp()}
-  delay(1000)
-
-  left(225);
-  delay(1000)
-  stp()}
-  delay(1000)
-
-  right(225);
-  delay(1000)
-  stp()}
-  delay(1000)
-
-  stp(225);
-  delay(1000)
-  stp()}
-  delay(1000)*/
-
+void loop() {
+  checkBluetooth();
+  Move();
 }
-
-
 void forward(int speed) {
   analogWrite(5, 0);
   analogWrite(6, speed);
   analogWrite(9, speed);
   analogWrite(10, 0);
 }
-
 
 
 void backward(int speed) {
@@ -60,112 +32,101 @@ void backward(int speed) {
   analogWrite(10, speed);
 }
 
-
-
-
 void left(int speed) {
-  analogWrite(5, speed);
-  analogWrite(6, 0);
-  analogWrite(9, speed);
-  analogWrite(10, 0);
+  analogWrite(5, 0);
+  analogWrite(6, speed);
+  analogWrite(9, 0);
+  analogWrite(10, speed);
 }
-
-
-
-
 void right(int speed) {
   analogWrite(5, speed);
   analogWrite(6, 0);
   analogWrite(9, speed);
   analogWrite(10, 0);
 }
-
-
-
-
-void stp(int speed) {
+void stp() {
   analogWrite(5, 0);
   analogWrite(6, 0);
   analogWrite(9, 0);
   analogWrite(10, 0);
-  }
+}
 
-
-
-void checkLineSensrs(int left, int right){
-  Serial.print("Left: "  + (String)digitalRead(left));
-  Serial.println("right: "  + (String)digitalRead(right));
+void checkLineSensors(int left, int right) {
+  Serial.print("Left: " + (String)digitalRead(left) + " ");
+  Serial.println("Right: " + (String)digitalRead(right));
   delay(500);
 }
 
-void checkBlueTooth(){
-if(bt.available()){
-  value = bt.read():
-  Serial.println('Mesege from phone: " + (String)value);
+
+void checkBluetooth() {
+  if (bt.available()) {
+    value = bt.read();
+    Serial.println("message from phone:" + (String)value);
   }
 }
-}
 
-void forwardleft (int speed) {
+void forwardLeft(int speed) {
   analogWrite(5, 0);
   analogWrite(6, speed);
   analogWrite(9, 0);
   analogWrite(10, 0);
-
-  void forwardRigt (int speed) {
+}
+void forwardRight(int speed) {
   analogWrite(5, 0);
   analogWrite(6, 0);
   analogWrite(9, speed);
   analogWrite(10, 0);
+}
 
-  void backwardleft (int speed) {
+void backwardLeft(int speed) {
   analogWrite(5, speed);
   analogWrite(6, 0);
   analogWrite(9, 0);
   analogWrite(10, 0);
-
-  void backwardRigt (int speed) {
+}
+void backwardRight(int speed) {
   analogWrite(5, 0);
   analogWrite(6, 0);
   analogWrite(9, 0);
   analogWrite(10, speed);
+}
 
-  void Move () {
-    stwich(value){
-      case 'F'
-      forward(255);
-      break;
+void Move(){
+  switch(value){
+    case 'F':
+    forward(255);
+    break;
+
+    case 'B':
+    backward(255);
+    break;
     
-      case 'B'
-      backward(255);
-      break;
+    case 'R':
+    right(255);
+    break;
 
-      case 'R'
-      Right(255);
-      break;
+    case 'L':
+    left(255);
+    break;
 
-       case 'L'
-      left(255);
-      break;
+    case 'G':
+    forwardLeft(255);
+    break;
 
-       case 'I'
-      forwardRight(255);
-      break;
+    case 'I':
+    forwardRight(255);
+    break;
 
-         case 'G'
-      forwardleft(255);
-      break;
+    case 'H':
+    backwardLeft(255);
+    break;
 
-       case 'J'
-      backwardRight(255);
-      break;
-
-         case 'H'
-      backwardleft(255);
-      break;
-
-         case 'S'
-      stp();
-      break;
-
-      
+    case 'J':
+    backwardRight(255);
+    break;
+    
+    case 'S':
+    stp();
+    break;
+  }
+}
---
+++ SB16 Web Junior/гончаренко анастасія/lesson17/lesson 20/script.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function () {
+    $(".hideslov").click(function () {
+        $(this).hide("slow")
+    })
+    $(".hidefast").click(function () {
+        $(this).hide("fast")
+    })
+    $(".hide3s").click(function () {
+        $(this).hide(3000)
+    })
+    $(".hide1s").click(function () {
+        $(this).hide(1000, function(){
+            alert("AHTUNG")
+        })
+    })
+
+    $(".show").click(function () {
+        $(".b").show("slow")
+        
+    })
+
+    let isVisible = true
+    $(".header").click(function(){
+        isVisible = !isVisible
+        isVisible ? $("textBar").hide() : $(".textBar").show();
+        
+    })
+})
+
+
+
+
+
