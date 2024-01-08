71a8aa33eaa99a4926e4754c132acee774dc0ea7
Teremki16 <teremki16robocode@gmail.com>
2024-01-08 09:59:46 +0200

    Update at 08.01.2024

--- SB16 Web Junior/Senychak/lesson 18/script.js
+++ SB16 Web Junior/Senychak/lesson 18/script.js
@@ -17,14 +17,14 @@ $(document).ready(function () {
         $(".b").show("slow");
     });
 
-    let isVisible = true;
     $(".header").click(function () {
-        isVisible = !isVisible;
-        isVisible ? $(".textBar").hide() : $(".textBar").show();
-        if(isVisible){
-            $(".textBar").show()
-        }else{
-            $(".textBar").hide()
-        }
+        $(".textBar").fadeToggle(400);
+    });
+
+    let opacity = 1;
+    $(".fade").click(function () {
+        opacity -= 0.1;
+        if (opacity <= 0) opacity = 1;
+        $(this).fadeTo(200, opacity);
     });
 });
