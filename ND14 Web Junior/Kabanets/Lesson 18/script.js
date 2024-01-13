f1d19978b0aca122aa2d53e5eca398592ad02a09
Teremki16 <teremki16robocode@gmail.com>
2024-01-13 08:39:40 +0200

    Update at 13.01.2024

---
+++ ND14 Web Junior/Kabanets/Lesson 18/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $(".item:nth-child(1)").click(function(){
+        $(this).hide("fast")
+    })
+    $(".item:nth-child(2)").click(function(){
+        $(this).hide("slow")
+    })
+    $(".item:nth-child(3)").click(function(){
+        $(this).hide(3000)
+    })
+    $(".item:nth-child(4)").click(function(){
+        $(this).hide(1000, function(){
+            alert("Я заховався!")
+        })
+     $(".item:nth-child(5)").click(function(){
+            $(".item").show("fast")
+        })
+    })
+    $(".header").click(function(){
+        $(".text").fadeToggle("fast")
+    })
+
+    let opacity = 1
+    $(".fade").click(function(){
+        opacity -= 0.1
+        $(this).fadeTo(100, opacity);
+        $(this).fadeIn(100, opacity)
+    })
+
+
+
+
+
+
+
+
+})
