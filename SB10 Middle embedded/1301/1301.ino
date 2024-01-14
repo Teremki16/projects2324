22c895318d36b5afd07e65814dec08eef39d80af
Teremki16 <teremki16robocode@gmail.com>
2024-01-14 08:33:07 +0200

    Update at 14.01.2024

---
+++ SB10 Middle embedded/1301/1301.ino
@@ -0,0 +1,28 @@
+int data[3];
+int index = 0;
+int pass[3] = {1, 2, 3};
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello Maxvell!");
+}
+
+void loop() {// put your
+  if (Serial.available() > 3) {
+    data[index] = Serial.parseInt();
+    index++;
+    Serial.print("*");
+
+  }
+  if (index == 3) {
+    Serial.println();
+    for (int i = 0; i < 3; i++) {
+      Serial.print(data[i]);
+      if (pass[i] != data[i]) {
+        Serial.print("Wrong Password!");
+        break;
+      }
+    }
+    index = 0;
+  }
+}
