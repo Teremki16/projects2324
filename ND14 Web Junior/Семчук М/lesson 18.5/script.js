554323f669315d064cb05cc659ad4c6e137c2e83
Teremki16 <teremki16robocode@gmail.com>
2024-01-20 08:26:47 +0200

    Update at 20.01.2024

---
+++ ND14 Web Junior/Семчук М/lesson 18.5/script.js
@@ -0,0 +1,23 @@
+$(".mistake").hide()
+
+$("#password").change(function(){
+    let pass = $(this).val();
+    $(".mistake").hide();
+    if (pass.lenght < 8){
+        $(".mistake:nth-child(4)").show()
+    }
+    if (!pass.match(/[A-z]/)){
+        $(".mistake:nth-child(1)").show()
+    }
+    if (!pass.match(/[A-Z]/)){
+        $(".mistake:nth-child(2)").show()
+    }
+    if (!pass.match(/[0-9]/)){
+        $(".mistake:nth-child(3)").show()
+    }
+});
+
+
+$(".header").click(function(){
+    $(".content").slideToggle("fast")
+})
---
+++ ND14 Web Junior/Семчук М/lesson 19/script.js
@@ -0,0 +1,7 @@
+$("button").click(function(){
+    $(".box").animate({
+        height: "300px",
+        width: "300px",
+        opacity:"0.5"
+    })
+})
