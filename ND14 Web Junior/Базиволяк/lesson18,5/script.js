37ca0ae3b30d9bda978a8380d95a20124fd79375
Teremki16 <teremki16robocode@gmail.com>
2024-01-20 08:21:08 +0200

    Update at 20.01.2024

---
+++ ND14 Web Junior/Базиволяк/lesson18,5/script.js
@@ -0,0 +1,24 @@
+$(".mistake").hide()
+
+$("#password").change(function(){
+
+    let pass = $(this).val()
+    $(".mistake").hide();
+    if(!pass.match(/[A-z]/)){
+        $(".mistake:nth-child(1)").show()
+    }
+    if(!pass.match(/[A-Z]/)){
+        $(".mistake:nth-child(2)").show()
+    }
+    if(!pass.match(/[0-9]/)){
+        $(".mistake:nth-child(3)").show()
+    }
+    if(pass.length<8){
+        $(".mistake:nth-child(4)").show()
+    }
+})
+
+
+$(".header").click(function(){
+    $(".content").slideToggle("fast");
+})
