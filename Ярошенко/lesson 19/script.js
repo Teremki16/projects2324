93be42b3502ab5ca0cdcb996f43d83ccc853f093
Teremki16 <teremki16robocode@gmail.com>
2024-01-27 09:07:05 +0200

    Update at 27.01.2024

--- Ярошенко/lesson 19/script.js
+++ Ярошенко/lesson 19/script.js
@@ -1,7 +1,22 @@
 $("button").click(function(){
     $(".box").animate ({
         height: "300px",
-        width: "300px",
         opacity: "0.5"
     })
+    $(".box").animate ({
+        width: "300px",
+        opacity: "1"
+    })
+    $(".box").animate ({
+        height: "100px",
+        opacity: "0.5"
+    })
+    $(".box").animate ({
+        width: "100px",
+        opacity: "1"
+    })
 })
+
+
+
+
---
+++ Ярошенко/lesson 20/script.js
@@ -0,0 +1,38 @@
+$(".text").click(function(){
+    alert($("h1").text());
+})
+$(".html").click(function(){
+    alert($("h1").html());
+})
+
+$("#setlink").click(function(){
+    let value = $("#text").val()
+    $("a").attr("href,http:// + value")
+    $("#text").val("")
+})
+
+let color = {
+    r: 0,
+    g: 0,
+    b: 0
+}
+
+function updateColor(){
+    $("body").css({background: `rgb(${color.r}, ${color.g}, ${color.b})`})
+}
+
+$(".range.r").on("change", function(){
+    color.r = $(this).val()
+    $(".t.r").val(color.r)
+    updateColor()
+})
+$(".range.g").on("chage.", function(){
+    color.g = $(this).val()
+    $(".t.g").val(color.g)
+    updateColor()
+})
+$(".range.b").on("chage.", function(){
+    color.b = $(this).val()
+    $(".t.b").val(color.b)
+    updateColor()
+})
