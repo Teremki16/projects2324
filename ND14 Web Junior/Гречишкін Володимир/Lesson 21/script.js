fb621622ecb534395adacd5fac3db2ea6751f85d
Teremki16 <teremki16robocode@gmail.com>
2024-02-03 09:22:50 +0300

    Update at 03.02.2024

---
+++ ND14 Web Junior/Гречишкін Володимир/Lesson 21/script.js
@@ -0,0 +1,28 @@
+$(".append").on("click",()=>{
+    $("ol").append("<li>Append item</li>")
+})
+$(".prepend").on("click",()=>{
+    $("ol").prepend("<li>Prepend item</li>")
+})
+
+
+let team1=1
+let team2=1
+
+$("#t1").on("click",()=>{
+    let name = $("input").val()
+    $(".tt1:last-child").after(`<div class ='tt1'>${team1}.${name}</div> `)
+     $("input").val("")
+     team1++     
+})
+$("#t2").on("click",()=>{
+    let name = $("input").val()
+    $(".tt2:last-child").after(`<div class ='tt2'>${team2}.${name}</div> `)
+     $("input").val("")
+     team2++     
+})
+
+$(".remBtn").on("click",()=>{
+    let elem=$(".remVal").val()
+    $(elem).remove()
+})
---
+++ SB16 Web Junior/lesson_20/script.js
@@ -0,0 +1,27 @@
+// $(".append").on("click", function(){
+//     $("ol").append("<li>Append item </li>")
+// })
+// $(".prepend").on("click", function(){
+//     $("ol").prepend("<li>Prepend item </li>")
+// })
+
+// let team1 = 1
+// let team2 = 1
+
+// $(".t1").on("click", ()=>{
+//     let name = $("input").val()
+//     $(".tt1:last-child").after(`<div class="tt1">${team1}. ${name}</div>`)
+//     $("input").val("")
+//     team1++
+// })
+// $(".t2").on("click", ()=>{
+//     let name = $("input").val()
+//     $(".tt2:last-child").after(`<div class="tt2">${team2}. ${name}</div>`)
+//     $("input").val("")
+//     team2++
+// })
+
+$(".remove").on("click", function(){
+    let elem = $("input[type='text']").val()
+    $(elem).remove()
+})
