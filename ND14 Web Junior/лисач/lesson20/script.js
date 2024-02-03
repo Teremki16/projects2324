5f47d1aed689428b7edebc3921e81ae0dd1e8790
Teremki16 <teremki16robocode@gmail.com>
2024-02-03 08:27:04 +0200

    Update at 03.02.2024

--- ND14 Web Junior/лисач/lesson20/script.js
+++ ND14 Web Junior/лисач/lesson20/script.js
@@ -10,3 +10,29 @@ $("#setlink").click(function(){
     $("a").attr("href", "httr://" + value)
     $("#text").val("")
 })
+
+let color = {
+    r: 0,
+    g: 0,
+    b: 0
+}
+
+function updateColor(){
+    $("body").css({background: `rgb(${color.r}, ${color.g}, ${color.b})`})
+}
+
+$(".range.r").on("change", function(){
+    color.r = $(this).val()
+    $(".t.r").val(color.r)
+    updateColor()
+})
+$(".range.g").on("change", function(){
+    color.g = $(this).val()
+    $(".t.g").val(color.g)
+    updateColor()
+})
+$(".range.b").on("change", function(){
+    color.b = $(this).val()
+    $(".t.b").val(color.b)
+    updateColor()
+})
---
+++ SB12 Web Beginner/dondorenko/lesson 20/script.js
@@ -0,0 +1,3 @@
+document.querySelector(".button").addEventListener("click", function(){
+    document.querySelector("nav").classList.toggle("open")
+})
