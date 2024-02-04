774189b54c1d949a85b93f773e43d690371d7050
Teremki16 <teremki16robocode@gmail.com>
2024-02-04 08:59:12 +0200

    Update at 04.02.2024

---
+++ SB16 Web Junior/Senychak/lesson 21_2/script.js
@@ -0,0 +1,13 @@
+let number = 1;
+
+$("button").on("click", () => {
+    let v = $("input").val();
+    $("input").val("");
+    $(".todolist").append(`<li class="i${number}">${v}</li>`);
+    number++;
+});
+
+$("html").on("click", "li", function(){
+    $(this).fadeOut(1000)
+    setTimeout(()=> $(this).remove(), 1000);
+})
