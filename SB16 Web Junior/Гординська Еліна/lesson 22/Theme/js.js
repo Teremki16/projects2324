ded350034754466604eeba6466189784f9f028e4
Teremki16 <teremki16robocode@gmail.com>
2024-02-04 08:55:37 +0200

    Update at 04.02.2024

---
+++ SB16 Web Junior/Гординська Еліна/lesson 22/Theme/js.js
@@ -0,0 +1,16 @@
+let theme = true; //true == light , false == dark
+
+$(".theme").on("click", function(){
+    theme = !theme
+    if(theme){
+        $(this).addClass("light")
+        $(this).html(`<i class="fa-solid fa-sun"></i>`)
+        $("body").css("background", 'linear-gradient(270deg, #2376ae 0%, #c16ecf 100%)')
+        $("footer, header, section").css('background', "rgba(0, 0, 0, 0.2)")
+    }else {
+        $(this).addClass("dark")
+        $(this).html(`<i class="fa-solid fa-moon"></i>`) 
+        $("body").css("background", 'linear-gradient(270deg, #0f3956 0%, #994aa7 100%)')
+        $("footer, header, section").css('background', "rgba(225, 255, 255, 0.2)")
+    }
+})
