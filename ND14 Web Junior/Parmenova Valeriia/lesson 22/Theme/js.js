7ee78af915c70b015c2f56124475047b71c41068
Teremki16 <teremki16robocode@gmail.com>
2024-02-10 08:28:27 +0200

    Update at 10.02.2024

---
+++ ND14 Web Junior/Parmenova Valeriia/lesson 22/Theme/js.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    let t = "light";
+    $(".theme").on("click", function(){
+        if(t == "light"){
+            t = "dark"
+            $(this).html(`<i class="fa-solid fa-moon"></i>`)
+            $("header h1 , section, footer, a").addClass("dark")
+            $(this).css("background", "rgba(255,255,255,0.2)")
+            $("body").css("background", "linear-gradient(270deg, #27476a 0%, #c16ecf 100%)")
+        }else{
+            t = "light"
+            $(this).html(`<i class="fa-solid fa-sun"></i>`)
+            $("header h1 , section, footer, a").removeClass("dark")
+            $(this).css("background", "rgba(0,0,0,0.2)")
+            $("body").css("background", "linear-gradient(270deg, #2376ae 0%, #c16ecf 100%)")
+        }
+    })
+})
