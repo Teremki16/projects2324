cb1fc03b1172f0a7732503579e5d9755f4d5fa81
Teremki16 <teremki16robocode@gmail.com>
2024-02-11 08:21:06 +0200

    Update at 11.02.2024

---
+++ SB16 Web Junior/Гординська Еліна/lesson 23/script.js
@@ -0,0 +1,24 @@
+function rnd(max){
+    return Math.floor(Math.random() * max);
+}
+
+// $(".petro").on("mouseenter", function(){
+//     $(this).css({
+//         "top": rnd(95) + "vh",
+//         "left": rnd(95) + "vw",
+//         "background-color": `rgb(${rnd(225)}, ${rnd(225)}, ${rnd(225)})`
+//     })
+// })
+
+$(".top").on("click", function(){
+    $(".petro").css({"top": "-=50px"})
+})
+$(".bottom").on("click", function(){
+    $(".petro").css({"top": "+=50px"})
+})
+$(".left").on("click", function(){
+    $(".petro").css({"left": "-=50px"})
+})
+$(".right").on("click", function(){
+    $(".petro").css({"left": "+=50px"})
+})
