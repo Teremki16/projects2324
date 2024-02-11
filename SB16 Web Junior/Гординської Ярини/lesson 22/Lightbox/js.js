16930f8c67d19487df52706e83be4ac45a330df3
Teremki16 <teremki16robocode@gmail.com>
2024-02-11 08:21:47 +0200

    Update at 11.02.2024

---
+++ SB16 Web Junior/Гординської Ярини/lesson 22/Lightbox/js.js
@@ -0,0 +1,13 @@
+$(document).ready(()=>{
+
+$(".item").on("click", function(){
+    let src = $(this).children("img").attr("src")
+    $(".overlay img").attr("src", src)
+    $(".overlay").addClass("open")
+})
+ $(".close").on("click", function(){
+    $(".overlay").removeClass("open")
+ })
+
+})
+
