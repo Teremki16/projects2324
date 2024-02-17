3ed6f55d4b945d6419ac5e5a1f4e44278e4b1171
Teremki16 <teremki16robocode@gmail.com>
2024-02-17 09:34:00 +0200

    Update at 17.02.2024

---
+++ ND14 Web Junior/лисач/lesson23/script.js
@@ -0,0 +1,24 @@
+// function rnd(max){
+//     return Math.floor(Math.random() * max);
+// }
+
+// $(".sharik").on("mouseenter", function(){
+//     $(this).css({
+//         "top" : rnd(95) + "vh",
+//         "left" : rnd(95) + "vw",
+//         "background" : `rgb(${rnd(255)},${rnd(255)},${rnd(255)})`,
+//     })
+// })
+
+$(".top").on("click", function(){
+    $(".sharik").css({"top": "-=50px"})
+})
+$(".bottom").on("click", function(){
+    $(".sharik").css({"top": "+=50px"})
+})
+$(".left").on("click", function(){
+    $(".sharik").css({"left": "-=50px"})
+})
+$(".right").on("click", function(){
+    $(".sharik").css({"left": "+=50px"})
+})
