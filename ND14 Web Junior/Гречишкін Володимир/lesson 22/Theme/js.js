4f6b042348f34192a704fff87e0cfa47bc064ef2
Teremki16 <teremki16robocode@gmail.com>
2024-02-17 10:37:14 +0300

    Update at 17.02.2024

--- ND14 Web Junior/Гречишкін Володимир/lesson 22/Theme/js.js
+++ ND14 Web Junior/Гречишкін Володимир/lesson 22/Theme/js.js
@@ -1,18 +1,10 @@
-$(document).ready(function(){
-    let t = "light";
-    $(".theme").on("click",function(){
-        if(t == "light"){
-            t ="dark"
-            $(this).html(`<i class="fa-solid fa-sun"></i>`)
-            $("header h1,section,footer,a").addClass("dark")
-            $(this).css("background","rgba(255,255,255,0.2)")
-            $("body").css("background", "linear-gradient(270deg, #103557 0%, #322335 100%)")
-        }else{
-            t = "light"
-            $(this).html(`<i class="fa-solid fa-moon"></i>`)
-            $("header h1,section,footer,a").removeClass("dark")
-            $(this).css("background","rgba(0,0,0,0.2)")
-            $("body").css("background", "linear-gradient(270deg, #2376ae 0%, #c16ecf 100%)")
-        }
+$(document).ready(()=>{
+    $(".item").on ("click",function(){
+        let src = $(this).children("img").attr("src")
+        $(".overlay img").attr("src",src);
+        $(".overlay").addClass("open")
+    });
+    $(".exit").on("click",function(){
+        $(".overlay").removeClass("open")
     })
-})
+});
