abc75de7bf2cc8435d3b46c76b8ea01c240929e9
Teremki16 <teremki16robocode@gmail.com>
2024-02-17 09:33:02 +0200

    Update at 17.02.2024

---
+++ Ярошенко/lesson 23/script.js
@@ -0,0 +1,19 @@
+function rnd(max) {
+    return Math.floor(Math.random() * max);
+}
+
+
+
+
+$(".sharik").on("mouseenter", function () {
+    $(this).css({
+        "top": rnd(95) + "vh",
+        "left": rnd(95) + "vw",
+        "background": `rgb(${rnd(255)}, ${rnd(255)}, ${rnd(255)})`,
+    })
+})
+
+
+$(".top").on("click", function(){
+    $(".sharik").css({"top": "-=50px"})
+})
