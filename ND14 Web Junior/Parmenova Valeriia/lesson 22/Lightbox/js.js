d84a7611cfab009c11b6fc69b49f5c42409879e8
Teremki16 <teremki16robocode@gmail.com>
2024-02-17 09:35:55 +0200

    Update at 17.02.2024

---
+++ ND14 Web Junior/Parmenova Valeriia/lesson 22/Lightbox/js.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+    $(".item").on("click", function () {
+        let src = $("this").children("img").attr("src")
+        $(".overlay img").attr("src",src)
+        $(".overlay").addClass("open")
+    })
+    $(".exit").on("click", function(){
+        $(".overlay").removeClass("open")
+    })
+})
