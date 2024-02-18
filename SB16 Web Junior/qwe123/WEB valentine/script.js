d8b67b286281f98ab0013a787aed66bf62dcdc81
Teremki16 <teremki16robocode@gmail.com>
2024-02-18 09:22:40 +0300

    Update at 18.02.2024

---
+++ SB16 Web Junior/qwe123/WEB valentine/script.js
@@ -0,0 +1,7 @@
+$("#heart").on("click", function(){
+    $("#top").animate({"top": "-100px"}, 1000);
+    $("#bottom").animate({"top": "400px"}, 1000, );
+    $("h3, p").animate({opacity: 1}, 1000);
+});
+
+
