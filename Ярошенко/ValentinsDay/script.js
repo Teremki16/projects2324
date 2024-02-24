6e14c03157a992c36124adc8111c34a048300c51
Teremki16 <teremki16robocode@gmail.com>
2024-02-24 08:23:54 +0200

    Update at 24.02.2024

---
+++ Ярошенко/ValentinsDay/script.js
@@ -0,0 +1,7 @@
+$("#heart").on("click", function(){
+//    console.log("gfg")
+    $("#top").animate({"top": "-140px"}, 1000)
+    $("#bottom").animate({"top": "280px"}, 1000)
+    $("p, h3").animate({"opacity": "1"}, 1000)
+}) 
+
