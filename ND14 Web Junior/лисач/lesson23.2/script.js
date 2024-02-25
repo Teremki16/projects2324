8450dcd0121db181dd726fb29a51f431bebead8e
Teremki16 <teremki16robocode@gmail.com>
2024-02-25 09:20:59 +0200

    Update at 25.02.2024

---
+++ ND14 Web Junior/лисач/lesson23.2/script.js
@@ -0,0 +1,16 @@
+$("button").on("click", function(){
+   let w = $("#width").val()
+   let h = $("#height").val()
+   if(w.search("%") != -1){
+    $(".box").width(w)
+   }else if (w.search("px") != -1){
+    $(".box").width(w.slice(0, -2))
+   }
+
+   if(h.search("%") != -1){
+    $(".box").height(h)
+   }else if (h.search("px") != -1){
+    $(".box").height(h.slice(0, -2))
+   }
+})
+
