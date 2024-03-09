1b6e9c1ec19408b3ccd1558aa3ba5d41aa0835fa
Teremki16 <teremki16robocode@gmail.com>
2024-03-09 08:45:16 +0200

    Update at 09.03.2024

--- ND14 Web Junior/лисач/lesson24.2/script.js
+++ ND14 Web Junior/лисач/lesson24.2/script.js
@@ -1,6 +1,32 @@
-let start;
-let andTime;
+let startTime;
+let endTime;
 
 $("button").on("click", ()=>{
     startTime = $.now()
+    exam(3)
 })
+
+function exam(q){
+    let a = Math.floor(Math.random()*100)
+    let b = Math.floor(Math.random()*100)
+    let answer = a + b
+    alertify.prompt(`Question ${Math.abs(q - 4)}:${a} + ${b} = ?`, function(e, val){
+        if(e){
+            if(answer == parseInt(val)){
+                alertify.success(`Correct: ${answer}`)
+                q--
+            }else{
+                alertify.error("wrong, try again!")
+            }
+            if(q == 0){
+                endTime = $.now();
+                let t = (endTime - startTime)/1000
+                alertify.alert(`You spend ${t} seconds!`)
+            }else{
+                exam(q)
+            }
+        }else{
+            return 0
+        }
+    })
+}
