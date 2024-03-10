5cb1805577e9d88c8ed83c0e693b887577c7a88b
Teremki16 <teremki16robocode@gmail.com>
2024-03-10 08:03:31 +0200

    Update at 10.03.2024

--- SB10 Middle embedded/Leshchenko_mar02/Leshchenko_mar02.ino
+++ SB10 Middle embedded/Leshchenko_mar02/Leshchenko_mar02.ino
@@ -5,13 +5,15 @@ int x[4] = {2, 3, 3, 2};
 int y[4] = {3, 3, 4, 4};
 void setup() {
   gb.begin(8);
+  randomSeed(analogRead(0));
+  playerCar(2, 12);
 
 }
 
 
 
 void loop() {
-  control();
+  enemyMove(100);
 }
 
 
@@ -23,8 +25,8 @@ void enemyCar(int x, int y) {
   gb.drawPoint(x - 1, y - 1);
   gb.drawPoint(x + 1, y - 1);
   gb.drawPoint(x, y - 2);
-  gb.drawPoint(x, y - 3);
-  gb.drawPoint(x, y - 3);
+  gb.drawPoint(x + 1, y - 3);
+  gb.drawPoint(x - 1, y - 3);
 
 }
 void playerCar(int x, int y) {
@@ -33,8 +35,8 @@ void playerCar(int x, int y) {
   gb.drawPoint(x - 1, y + 1);
   gb.drawPoint(x + 1, y + 1);
   gb.drawPoint(x, y + 2);
-  gb.drawPoint(x, y + 3);
-  gb.drawPoint(x, y + 3);
+  gb.drawPoint(x + 1, y + 3);
+  gb.drawPoint(x - 1 , y + 3);
 
 }
 void wipeplayerCar(int x, int y) {
@@ -43,8 +45,8 @@ void wipeplayerCar(int x, int y) {
   gb.wipePoint(x - 1, y + 1);
   gb.wipePoint(x + 1, y + 1);
   gb.wipePoint(x, y + 2);
-  gb.wipePoint(x, y + 3);
-  gb.wipePoint(x, y + 3);
+  gb.wipePoint(x + 1, y + 3);
+  gb.wipePoint(x - 1, y + 3);
 
 }
 void wipeenemyCar(int x, int y) {
@@ -53,19 +55,70 @@ void wipeenemyCar(int x, int y) {
   gb.wipePoint(x - 1, y - 1);
   gb.wipePoint(x + 1, y - 1);
   gb.wipePoint(x, y - 2);
-  gb.wipePoint(x, y - 3);
-  gb.wipePoint(x, y - 3);
+  gb.wipePoint(x + 1, y - 3);
+  gb.wipePoint(x - 1, y - 3);
 }
-  void control() {
-    if (gb.getKey() > 0) {
-      if (gb.getKey() == 4) {
-        wipeplayerCar(5, 12);
-        playerCar(2, 12);
-      }
-      if (gb.getKey() == 5) {
-        wipeplayerCar(2, 12);
-        playerCar(5, 12);
-      }
+void control() {
+  if (gb.getKey() > 0) {
+    if (gb.getKey() == 4) {
+      wipeplayerCar(5, 12);
+      playerCar(2, 12);
+    }
+    if (gb.getKey() == 5) {
+      wipeplayerCar(2, 12);
+      playerCar(5, 12);
+    }
 
-    }
-    }
+  }
+} int enemy_x = 2;
+void enemyMove(int enemySpeed) {
+  enemy_x = random(10);
+  if(enemy_x > 5){
+    enemy_x=2;
+  }else {
+    enemy_x= 5;
+  }
+  for (int enemy_y = 0; enemy_y < 16; enemy_y++) {
+    createLine(enemy_y);
+    createLine(enemy_y+5);
+    createLine(enemy_y+10);
+    createLine(enemy_y+15);
+
+     createLine(enemy_y-5);
+    createLine(enemy_y-10);
+    createLine(enemy_y-15);
+    enemyCar(enemy_x, enemy_y);
+    control();
+    delay(enemySpeed);
+    wipeenemyCar(enemy_x, enemy_y);
+    clearLine(enemy_y);
+    clearLine(enemy_y+5);
+    clearLine(enemy_y+10);
+    clearLine(enemy_y+15);
+
+    clearLine(enemy_y-5);
+    clearLine(enemy_y-10);
+    clearLine(enemy_y-15);
+  }
+}
+void createLine(int y){
+
+  gb.drawPoint(0, y);
+  gb.drawPoint(0, y+1);
+  gb.drawPoint(0, y+2);
+  
+  gb.drawPoint(7, y);
+  gb.drawPoint(7, y+1);
+  gb.drawPoint(7, y+2);
+}
+
+void clearLine(int y){
+
+  gb.wipePoint(0, y);
+  gb.wipePoint(0, y+1);
+  gb.wipePoint(0, y+2);
+  
+  gb.wipePoint(7, y);
+  gb.wipePoint(7, y+1);
+  gb.wipePoint(7, y+2);
+}
