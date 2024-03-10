6573fbb3f6d116e00d7766ce2de1fc0d0859a7b9
Teremki16 <teremki16robocode@gmail.com>
2024-03-10 08:01:21 +0200

    Update at 10.03.2024

--- SB14 Middle embedded/racing/racing.ino
+++ SB14 Middle embedded/racing/racing.ino
@@ -1,21 +1,15 @@
 #include <GameBoy.h>
 GameBoy gb;
 
-int x[4] = {2, 3, 3, 2};
-int y[4] = {3, 3, 4, 4};
-
 void setup() {
-  gb.begin(5);
-  randomSeed(analogRead(0));
+  gb.begin(8);
   playerCar(2, 12);
+  randomSeed(analogRead(0));
 }
-
 
 void loop() {
-  enemyMove(100);
-
+  mainRacing(100);
 }
-
 void enemyCar(int x, int y) {
   gb.drawPoint(x, y);
   gb.drawPoint(x, y - 1);
@@ -35,17 +29,18 @@ void playerCar(int x, int y) {
   gb.drawPoint(x - 1, y + 3);
   gb.drawPoint(x + 1, y + 3);
 }
-void wipeEnemyCar(int x, int y) {
-  gb. wipePoint(x, y);
-  gb. wipePoint(x, y - 1);
-  gb. wipePoint(x - 1, y - 1);
-  gb. wipePoint(x + 1, y - 1);
-  gb. wipePoint(x, y - 2);
-  gb. wipePoint(x - 1, y - 3);
-  gb. wipePoint(x + 1, y - 3);
 
+void wipeEnemyCar(int x, int y) {
+  gb.wipePoint(x, y);
+  gb.wipePoint(x, y - 1);
+  gb.wipePoint(x - 1, y - 1);
+  gb.wipePoint(x + 1, y - 1);
+  gb.wipePoint(x, y - 2);
+  gb.wipePoint(x - 1, y - 3);
+  gb.wipePoint(x + 1, y - 3);
 }
-void wipeplayerCar(int x, int y) {
+
+void wipePlayerCar(int x, int y) {
   gb.wipePoint(x, y);
   gb.wipePoint(x, y + 1);
   gb.wipePoint(x - 1, y + 1);
@@ -58,23 +53,23 @@ void wipeplayerCar(int x, int y) {
 void control() {
   if (gb.getKey() > 0) {
     if (gb.getKey() == 4) {
-      wipeplayerCar(5, 12);
+      wipePlayerCar(5, 12);
       playerCar(2, 12);
     }
     if (gb.getKey() == 5) {
-      wipeplayerCar(2, 12);
+      wipePlayerCar(2, 12);
       playerCar(5, 12);
     }
-
   }
 }
+
 int enemy_x = 2;
-void enemyMove(int enemySpeed) {
+void mainRacing(int enemySpeed) {
   enemy_x = random(10);
   if (enemy_x > 5) {
-    enemy_x = 2;
-  } else {
     enemy_x = 5;
+  } else {
+    enemy_x = 2;
   }
   for (int enemy_y = 0; enemy_y < 16; enemy_y++) {
     createLine(enemy_y);
@@ -84,7 +79,7 @@ void enemyMove(int enemySpeed) {
     createLine(enemy_y - 5);
     createLine(enemy_y - 10);
     createLine(enemy_y - 15);
-    enemyCar(enemy_x,  enemy_y);
+    enemyCar(enemy_x, enemy_y);
     control();
     delay(enemySpeed);
     wipeEnemyCar(enemy_x, enemy_y);
@@ -100,19 +95,21 @@ void enemyMove(int enemySpeed) {
 
 void createLine(int y) {
   gb.drawPoint(0, y);
-  gb.drawPoint(0, y  + 1);
+  gb.drawPoint(0, y + 1);
   gb.drawPoint(0, y + 2);
 
   gb.drawPoint(7, y);
-  gb.drawPoint(7, y  + 1);
+  gb.drawPoint(7, y + 1);
   gb.drawPoint(7, y + 2);
 }
+
 void clearLine(int y) {
   gb.wipePoint(0, y);
-  gb.wipePoint(0, y  + 1);
+  gb.wipePoint(0, y + 1);
   gb.wipePoint(0, y + 2);
 
   gb.wipePoint(7, y);
-  gb.wipePoint(7, y  + 1);
+  gb.wipePoint(7, y + 1);
   gb.wipePoint(7, y + 2);
+
 }
