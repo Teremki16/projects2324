bc914be8e35d5d10779bb86f4978f7159c6fb140
Teremki16 <teremki16robocode@gmail.com>
2024-03-16 08:36:26 +0200

    Update at 16.03.2024

---
+++ ND14 Web Junior/kalashnikov/lesso25/script.js
@@ -0,0 +1,49 @@
+$("#h").knob({
+    max:24,
+    width:300,
+    height:300,
+    displayInput: false,
+    thickness: 0.3,
+    bgColor:"gray",
+    fgColor: "yellow",
+    readOnly: true,
+})
+
+$("#m").knob({
+    max:60,
+    width:200,
+    height:300,
+    displayInput: false,
+    bgColor:"gray",
+    fgColor:"skyblue",
+    readOnly: true,
+})
+
+$("#s").knob({
+    max:60,
+    width:100,
+    height:100,
+    displayInput: false,
+    thickness: 0.6,
+    bgColor:"gray",
+    fgColor:"lightgreen",
+    readOnly: true,
+})
+
+function clock(){
+    let d = new Date()
+    let $h = $("#h")
+    let $m = $("#m")
+    let $s = $("#s")
+    let h = d.getHours()
+    let m = d.getMinutes()
+    let s = d.getSeconds()
+    $h.val(h).trigger("change")
+    $m.val(m).trigger("change")
+    $s.val(s).trigger("change")
+    $(".h").text(h)
+    $(".m").text(m)
+    $(".s").text(s)
+    setTimeout("clock()", 1000)
+}
+clock()
