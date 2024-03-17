20598e6a307bd4e9101af314aa1e72704ffa538c
Teremki16 <teremki16robocode@gmail.com>
2024-03-17 10:06:41 +0300

    Update at 17.03.2024

--- SB16 Web Junior/Sidlyarenko/lesson_11/script.js
+++ SB16 Web Junior/Sidlyarenko/lesson_11/script.js
@@ -1,63 +1,54 @@
-let canvas = document.querySelector("canvas")
-let ctx = canvas.getContext("2d")
+let canvases = document.querySelectorAll("canvas")
+let ctx = []
 
-let colorPicker = document.getElementById("color")
-let renge = document.getElementById("range")
-let rangeText = document.getElementById("rangeValue")
-
-let lw = 10
-let color = "black"
-let isMouseDown = false
-
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-
-function fillin(){
-    ctx.fillStyle = color
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
+for(let i = 0; i < canvases.length; i++){
+    canvases[i].width=700
+    canvases[i].height=500
+    ctx.push(canvases[i].getContext("2d"))
 }
 
-function clearAll(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-}
 
-colorPicker.addEventListener("change", (event)=>{
-    color = event.target.value
-})
+ctx[0].moveTo(200,200)
+ctx[0].lineTo(370,400)
+ctx[0].lineTo(400,170)
+ctx[0].lineWidth = 3
+ctx[0].stroke()
 
-canvas.addEventListener("mousedown", ()=>{
-    isMouseDown = true
-    ctx.beginPath()
-})
+ctx[1].beginPath()
+ctx[1].arc(350, 250, 100, (30 * Math.PI) / 180, (340 * Math.PI) / 180)
+ctx[1].lineTo(350, 250)
+ctx[1].fillStyle = "yellow"
+ctx[1].lineWidth = 5
+ctx[1].closePath()
+ctx[1].fill()
+ctx[1].stroke()
+ctx[1].beginPath()
+ctx[1].fillStyle = "black"
+ctx[1].arc(380, 200, 10, 0, 2 * Math.PI)
+ctx[1].closePath()
+ctx[1].fill()
 
-canvas.addEventListener("mouseup", ()=>{
-    isMouseDown = false
-})
+ctx[2].strokeRect(215, 115, 266, 266)
+ctx[2].strokeRect(218, 118, 260, 260)
+ctx[2].fillStyle = "orange"
+ctx[2].fillRect(220, 120, 256, 256)
+ctx[2].fillStyle = "black"
 
-canvas.addEventListener("mousemove", (event)=>{
-    if(isMouseDown){
-        ctx.fillStyle = color
-        ctx.strokeStyle = color
-        ctx.lineWidth = lw
-
-        ctx.lineTo(event.clientX, event.clientY)
-        ctx.stroke()
-
-        ctx.beginPath()
-        ctx.arc(event.clientX, event.clientY, lw / 2, 0, 2 * Math.PI)
-        ctx.fill()
-
-        ctx.beginPath()
-        ctx.moveTo(event.clientX, event.clientY)
+for(let i = 0; i  < 8; i+=2 ){
+    for(let j = 0; j  < 8; j+=2 ){
+        ctx[2].fillRect(220 + i * 32, 120 + j *32, 32, 32)
+        ctx[2].fillRect(252 + i * 32, 152 + j *32, 32, 32)
     }
-})
+}
 
-range.addEventListener("input", (event)=>{
-    lw = event.target.value
-    rangeText.innerText = lw
-})
 
-document.querySelector("a").addEventListener("click", ()=>{
-    let url = canvas.toDataURL().replace("image/png", "image/octet-stream")
-    document.querySelector("a").href = url
-})
+ctx[3].fillStyle = "white"
+ctx[3].beginPath()
+ctx[3].arc(200, 200, 100, 0, 2 * Math.PI)
+ctx[3].closePath()
+ctx[3].fill()
+
+ctx[3].beginPath()
+ctx[3].arc(250, 50, 100, 0, 2 * Math.PI)
+ctx[3].closePath()
+ctx[3].fill()
---
+++ SB16 Web Junior/Sidlyarenko/lesson_26/script.js
@@ -0,0 +1,23 @@
+
+let controller = new ScrollMagic.Controller()
+
+let headerScene = new ScrollMagic.Scene({
+    triggerElement: "header",
+    triggerHook: 0,
+    duration: "10%"
+}).setPin("header", {
+    pushFollowers: false
+}).addIndicators().addTo(controller)
+
+$("section").each(function () {
+    let scene1 = new ScrollMagic.Scene({
+        triggerElement: this,
+        duration: "200%",
+        triggerHook: 0,
+    })
+        .setPin(this, {
+            pushFollowers: false
+        })
+        .addIndicators()
+        .addTo(controller)
+})
