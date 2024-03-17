34c3837717ede08286567e50c6f2ce6cab4c9ebb
Teremki16 <teremki16robocode@gmail.com>
2024-03-17 09:07:07 +0200

    Update at 17.03.2024

--- SB14 Middle embedded/racing/racing.ino
+++ SB14 Middle embedded/racing/racing.ino
@@ -49,20 +49,22 @@ void wipePlayerCar(int x, int y) {
   gb.wipePoint(x - 1, y + 3);
   gb.wipePoint(x + 1, y + 3);
 }
-
+int player_x = 2;
+int player_y = 12;
 void control() {
   if (gb.getKey() > 0) {
     if (gb.getKey() == 4) {
       wipePlayerCar(5, 12);
-      playerCar(2, 12);
+      player_x = 2;
     }
     if (gb.getKey() == 5) {
       wipePlayerCar(2, 12);
-      playerCar(5, 12);
+      player_x = 5;
     }
   }
+  playerCar(player_x, player_y);
 }
-
+int gameSpeed = 100;
 int enemy_x = 2;
 void mainRacing(int enemySpeed) {
   enemy_x = random(10);
@@ -71,6 +73,7 @@ void mainRacing(int enemySpeed) {
   } else {
     enemy_x = 2;
   }
+
   for (int enemy_y = 0; enemy_y < 16; enemy_y++) {
     createLine(enemy_y);
     createLine(enemy_y + 5);
@@ -80,6 +83,17 @@ void mainRacing(int enemySpeed) {
     createLine(enemy_y - 10);
     createLine(enemy_y - 15);
     enemyCar(enemy_x, enemy_y);
+    if (Collision(player_x, player_y, enemy_x, enemy_y)) {
+      gb.testMatrix(10);
+      //      gb.sound(COLLISION);
+      return;
+    }
+
+        if(enemy_y > 12){
+    //      gb.sound(SCORE);
+    gameSpeed -= 1;
+    
+        }
     control();
     delay(enemySpeed);
     wipeEnemyCar(enemy_x, enemy_y);
@@ -112,4 +126,14 @@ void clearLine(int y) {
   gb.wipePoint(7, y + 1);
   gb.wipePoint(7, y + 2);
 
+}
+
+bool Collision(int player_x, int player_y, int enemy_x, int enemy_y) {
+  if (player_x == enemy_x && player_y == enemy_y) {
+    return true;
+  }
+  if (player_x == enemy_x && enemy_y > 12) {
+    return true;
+  }
+  return false;
 }
