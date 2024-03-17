985e445d22394d2d41acb4a3f7f16747263d6831
Teremki16 <teremki16robocode@gmail.com>
2024-03-17 09:07:16 +0200

    Update at 17.03.2024

--- SB10 Middle embedded/Yershov/car_game_boy/car_game_boy.ino
+++ SB10 Middle embedded/Yershov/car_game_boy/car_game_boy.ino
@@ -2,15 +2,16 @@
 GameBoy chupacabra;
 
 
-int x [4] = {2, 3, 3, 2};
-int y [4] = {3, 3, 4, 4};
+
 void setup() {
   chupacabra.begin(8);
+  MyCar(2, 12);
+  randomSeed(analogRead(0));
 
 }
 
 void loop() {
-control();
+  MainRacing(100);
 
 }
 
@@ -52,16 +53,93 @@ void wipeMyCar(int x , int y) {
   chupacabra.wipePoint(x - 1, y + 3);
   chupacabra.wipePoint(x + 1, y + 3);
 }
+int player_x = 2;
+int player_y = 12;
 void control() {
   if (chupacabra.getKey() > 0) {
     if (chupacabra.getKey() == 4) {
       wipeMyCar(5, 12);
-      MyCar(2, 12);
+      player_x = 2;
     }
     if (chupacabra.getKey() == 5) {
       wipeMyCar(2, 12);
-      MyCar(5, 12);
+      player_x = 5;
     }
-    delay(100);
+  }
+  MyCar (player_x, player_y);
+}
+
+int enemy_x = 2;
+void MainRacing(int enemySpeed) {
+  enemy_x = random(100);
+  if (enemy_x > 50) {
+    enemy_x = 5;
+  } else {
+    enemy_x = 2;
+
+  }
+
+
+  for (int enemy_y = 0; enemy_y < 16; enemy_y++) {
+    createLine(enemy_y);
+    createLine(enemy_y + 5);
+    createLine(enemy_y + 10);
+    createLine(enemy_y + 15);
+    createLine(enemy_y - 5);
+    createLine(enemy_y - 10);
+    createLine(enemy_y - 15);
+    enemyCar(enemy_x, enemy_y);
+    if (Collision(player_x, player_y, enemy_x, enemy_y)) {
+     
+      chupacabra.sound(COLLISION);
+       chupacabra.testMatrix(10);
+       chupacabra.clearDisplay();
+       
+      return;
+    }
+    if (enemy_y>16){
+      
+      
+    }
+    control();
+    delay(enemySpeed);
+    wipeEnemyCar (enemy_x, enemy_y);
+    clearLine(enemy_y);
+    clearLine(enemy_y + 5);
+    clearLine(enemy_y + 10);
+    clearLine(enemy_y + 15);
+    clearLine(enemy_y - 5);
+    clearLine(enemy_y - 10);
+    clearLine(enemy_y - 15);
   }
 }
+void createLine(int y) {
+  chupacabra.drawPoint(0, y);
+  chupacabra.drawPoint(0, y + 1);
+  chupacabra.drawPoint(0, y + 2);
+
+  chupacabra.drawPoint(7, y);
+  chupacabra.drawPoint(7, y + 1);
+  chupacabra.drawPoint(0, y + 2);
+
+}
+void clearLine(int y) {
+  chupacabra.wipePoint(0, y);
+  chupacabra.wipePoint(0, y + 1);
+  chupacabra.wipePoint(0, y + 2);
+
+  chupacabra.wipePoint(7, y);
+  chupacabra.wipePoint(7, y + 1);
+  chupacabra.wipePoint(0, y + 2);
+
+}
+
+bool Collision(int player_x, int player_y, int enemy_x, int enemy_y) {
+  if (player_x == enemy_x && player_y == enemy_y) {
+    return true;
+  }
+  if (player_x == enemy_x && enemy_y > 12) {
+    return true;
+  }
+    return false;
+  }
