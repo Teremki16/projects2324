3d4bc1c971738fee86690ad58f0df5c65636aef6
Teremki16 <teremki16robocode@gmail.com>
2024-03-23 08:09:25 +0200

    Update at 23.03.2024

---
+++ ND14 Web Junior/Kabanets/Lesson 26/script.js
@@ -0,0 +1,32 @@
+let controller = new ScrollMagic.Controller();
+
+let headerScene = new ScrollMagic.Scene({
+    triggerElement: "header",
+    triggerHook: 0,
+    duration: "10%"
+})
+    .setPin("header",{
+        pushFollowers: false
+    })
+    .addTo(controller)
+
+let headerScene2 = new ScrollMagic.Scene({
+    triggerElement: ".s1",
+    triggerHook: 0.3,
+})
+    .setPin("header",{
+        pushFollowers: false
+    })
+    .addTo(controller)
+
+
+$("section").each(function(){
+    let scene1 = new ScrollMagic.Scene({
+    triggerElement: this,
+    duration: "200%",
+    triggerHook: 0,
+})
+    .setPin(this, {pushFollowers: false})
+    .addIndicators()
+    .addTo(controller);
+})
