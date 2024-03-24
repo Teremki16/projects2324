eb9fa2b44b3ee1cae918fc8a7b5f2787019ddb96
Teremki16 <teremki16robocode@gmail.com>
2024-03-24 08:58:02 +0200

    Update at 24.03.2024

--- ND14 Web Junior/лисач/lesson26/script.js
+++ ND14 Web Junior/лисач/lesson26/script.js
@@ -1,13 +1,30 @@
 let controller = new ScrollMagic.Controller();
 
+let headerScene = new ScrollMagic.Scene({
+    triggerElement: " header",
+    triggerHook: 0,
+    duration: "10%"
+}).setPin("header", {
+    pushFollowers: false
+}).addTo(controller)
+
+let headerScene2 = new ScrollMagic.Scene({
+    triggerElement: "s1",
+    triggerHook: 0.3,
+}).setPin("header", {
+    pushFollowers: false
+}).addTo(controller)
+
   
 
 $("section").each(function(){
     let scene1 = new ScrollMagic.Scene({
         triggerElement: this,
-        duration: "50%"
+        duration: "200%",
+        triggerHook: 0,
+        
     })
-    .setClassToggle(this, "fadeIN")
+    .setPin(this, {pushFollowers: false})
     .addIndicators()
     .addTo(controller)
 })
