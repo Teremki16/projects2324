a0de19191fc4a9899ad6ccdbb792d32df12e2400
Teremki16 <teremki16robocode@gmail.com>
2024-03-26 06:47:26 +0200

    Update at 26.03.2024

---
+++ ND14 Web Junior/Сеничак/lesson 27/script.js
@@ -0,0 +1,33 @@
+let controller = new ScrollMagic.Controller();
+
+let first_scene = new ScrollMagic.Scene({
+    triggerElement: ".s1",
+    triggerHook: 0.8,
+})
+    .setClassToggle(".s1 > div", "fade-right")
+    .addIndicators()
+    .addTo(controller);
+
+let second_scene = new ScrollMagic.Scene({
+    triggerElement: ".s2",
+    triggerHook: 0.8,
+})
+    .setClassToggle(".s2 > div", "fade-left")
+    .addIndicators()
+    .addTo(controller);
+
+let third_scene = new ScrollMagic.Scene({
+    triggerElement: ".s3",
+    triggerHook: 0.8,
+})
+    .setClassToggle(".s3 > div", "fade-right")
+    .addIndicators()
+    .addTo(controller);
+
+    let fourth_scene = new ScrollMagic.Scene({
+    triggerElement: ".s4",
+    triggerHook: 0.8,
+})
+    .setClassToggle(".s4 > div", "fade-left")
+    .addIndicators()
+    .addTo(controller);
