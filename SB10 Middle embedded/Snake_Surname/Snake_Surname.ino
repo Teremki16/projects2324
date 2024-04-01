7081ad644abc63021cf2c717350ba91febf2efef
Teremki16 <teremki16robocode@gmail.com>
2024-04-01 07:56:35 +0300

    Update at 01.04.2024

--- SB10 Middle embedded/Snake_Surname/Snake_Surname.ino
+++ SB10 Middle embedded/Snake_Surname/Snake_Surname.ino
@@ -7,6 +7,15 @@ int dirX, dirY;
 int snakeX[10];
 int snakeY[10];
 
+int up = 0;
+int right = 1;
+int bottom = 2;
+int left = 3;
+
+int direction = right;
+
+int lenSnake = 5;
+
 int foodX = 0, foodY = 0;
 
 void setup() {
@@ -18,43 +27,73 @@ void setup() {
 
 void loop() {
   makeMove();
-  dirX = dirX + x;
-  dirY = dirY + y;
-  if(dirX > 7) dirX = 0;
-  if(dirX < 0) dirX = 7;
-  if(dirY > 15) dirY = 0;
-  if(dirY < 0) dirY = 15;
-  if(dirX == foodX && dirY == foodY){
+  move();
+  if (snakeX[0] == foodX && snakeY[0] == foodY) {
     makeFood();
     tone(4, 3000, 100);
   }
   gb.clearDisplay();
   gb.drawPoint(foodX, foodY);
-  gb.drawPoint(dirX, dirY);
+  drawSnake();
   delay(300);
 }
 
-void makeMove(){
-  if(gb.getKey() == 3){
-    
-    x = 0;
-    y = -1;
+void makeMove() {
+  if (gb.getKey() == 3) {
+    direction = up;
   }
-  if(gb.getKey() == 6){
-    x = 0;
-    y = 1;
+  if (gb.getKey() == 6) {
+    direction = bottom;
   }
-  if(gb.getKey() == 4){
-    x = -1;
-    y = 0;
+  if (gb.getKey() == 4) {
+    direction = left;
   }
-  if(gb.getKey() == 5){
-    x = 1;
-    y = 0;
+  if (gb.getKey() == 5) {
+    direction = right;
   }
 }
 
-void makeFood(){
+void makeFood() {
   foodX = random(8);
   foodY = random(16);
+}
+
+void move() {
+  for (int i = lenSnake - 1; i > 0; i--) {
+    snakeX[i] = snakeX[i - 1];
+    snakeY[i] = snakeY[i - 1];
+  }
+  if(direction == up){
+    if(snakeY[0] == 0){
+      snakeY[0] = 15;
+    }else{
+      snakeY[0]--;
+    }
+  }
+  if(direction == bottom){
+    if(snakeY[0] == 15){
+      snakeY[0] = 0;
+    }else{
+      snakeY[0]++;
+    }
+  }
+  if(direction == left){
+    if(snakeX[0] == 0){
+      snakeX[0] = 7;
+    }else{
+      snakeX[0]--;
+    }
+  }if(direction == right){
+    if(snakeX[0] == 7){
+      snakeX[0] = 0;
+    }else{
+      snakeX[0]++;
+    }
+  }
+}
+
+void drawSnake(){
+  for(int i = 0; i < lenSnake; i++){
+    gb.drawPoint(snakeX[i], snakeY[i]);
+  }
 }
