1c37d9809dc519f6c3aeccc7f1917d979131bc9b
Teremki16 <teremki16robocode@gmail.com>
2024-04-06 08:10:03 +0300

    Update at 06.04.2024

--- ND14 Web Junior/Гречишкін Володимир/lesson27.2/script.js
+++ ND14 Web Junior/Гречишкін Володимир/lesson27.2/script.js
@@ -1,1 +1,19 @@
 let controller = new ScrollMagic.Controller();
+
+let pscene = new ScrollMagic.Scene({
+    triggerElement:".paralax1",
+    triggerHook:0.7,
+    duration: "200%"
+}).setTween(".bg1",1, {y: "80%",ease:Linear.easeNone}).addIndicators().addTo(controller)
+
+let pscene2 = new ScrollMagic.Scene({
+    triggerElement:".paralax2",
+    triggerHook:0.7,
+    duration: "200%"
+}).setTween(".bg2",1, {y: "80%",ease:Linear.easeNone}).addIndicators().addTo(controller)
+
+let pscene3 = new ScrollMagic.Scene({
+    triggerElement:".paralax3",
+    triggerHook:0.7,
+    duration: "200%"
+}).setTween(".bg3",1, {y: "80%",ease:Linear.easeNone}).addIndicators().addTo(controller)
