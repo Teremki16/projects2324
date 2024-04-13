eb65595a5215864e88f28751aca3aa6d1d1d047c
Teremki16 <teremki16robocode@gmail.com>
2024-04-13 08:42:56 +0300

    Update at 13.04.2024

---
+++ ND14 Web Junior/лисач/quiz/task1.js
@@ -0,0 +1,64 @@
+$(".rules").slideUp(0)
+
+let answers = [
+    "яблоко",
+    "груша",
+    "город",
+    "школа",
+    "сайт",
+    "браузер",
+    "плагін",
+    "колір",
+    "стиль",
+    "язик",
+    "візерунок",
+    "сорока"
+]
+
+let was = []
+
+let rebus = 0;
+
+let score = 0;
+
+$(".slideRules").on("click", function(){
+    $(".rules").slideToggle()
+})
+
+function rnd(){
+    return Math.floor(1+ Math.random() * 12);
+}
+
+function startRebus(num){
+    $(".img img").attr("src", `rebuses/${num}.jpg`)
+    rebus = num;
+}
+
+startRebus(rnd())
+
+$("#t1btn").on("click", function(){
+    let ans = $("#t1input").val().toLowerCase()
+    if(ans == answers[rebus - 1]){
+        alertify.success("Correct!")
+        score++;
+        $("#t1input").val("")
+        $("#progress").val(score).trigger("change")
+        was.push(rebus)
+        do{
+
+        }while(was.includes(rebus))
+        startRebus(rnd())
+    }else{
+        alertify.error("Wrong! Try again!")
+    }
+})
+
+$("#progress").knob({
+    min: 0,
+    max: 5,
+    angleArc: 120,
+    angleOffset: -60,
+    displayInput: false,
+    lineCap: "round",
+    readOnly: true
+})
