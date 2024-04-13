c8e2580ada5dbcf48ea5825b321213bcdec195da
Teremki16 <teremki16robocode@gmail.com>
2024-04-13 08:42:19 +0300

    Update at 13.04.2024

--- Ярошенко/ouiz/task.js
+++ Ярошенко/ouiz/task.js
@@ -1,4 +1,4 @@
-$(".rules").slideUP()
+$(".rules").slideUp(0)
 
 
 
@@ -17,4 +17,58 @@ let answers = [
     "шерлок",
     "відпустка",
     "аквапарк",
-]
+];
+
+let was = []
+
+let rebus = 0;
+
+let score = 0;
+
+
+
+function rnd(){
+    return Math.floor(1+ Math.random() * 12);
+}
+
+function startRebus(num){
+    $(".img img").attr("src", `rebuses/${num}.png`)
+    rebus = num;
+}
+
+startRebus(rnd())
+
+
+
+$("#t1btn").on("click", function(){
+    let ans = $("#t1input").val().toLowerCase()
+    if(ans == answers[rebus - 1]){
+        alertify.success("Correct!")
+        score++
+        $("#t1input").val("")
+        $("#progress").val(score).trigger("change")
+        was.push(rebus)
+        if (score < 5) {
+        do{
+            rebus = rnd()
+        }while(was.includes(rebus))
+        startRebus(rebus)
+    }else {
+        $(".nextTask").css("display", "block")
+    }
+    }else{
+        alertify.error("Wrong");
+    }
+})
+
+$("#progress").knob({
+    min: 0,
+    max: 5,
+    angleArc: 120,
+    angleOffset: -60,
+    displayInput: false,
+    LineVap: "round",
+    readOnly: true
+
+})
+
