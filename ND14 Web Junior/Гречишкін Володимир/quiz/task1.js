3a3c758ef2a5e985b7f52c496e6139e70d784dd6
Teremki16 <teremki16robocode@gmail.com>
2024-04-13 08:46:14 +0300

    Update at 13.04.2024

--- ND14 Web Junior/Гречишкін Володимир/quiz/task1.js
+++ ND14 Web Junior/Гречишкін Володимир/quiz/task1.js
@@ -1,20 +1,69 @@
 $(".rules").slideUp()
 
 let answers=[
-    "Бобер",
-    "Кросівок",
-    "Фар край",
-    "ГТА",
-    "Бігмак",
-    "Рукав",
-    "Меал гір",
-    "Піцерія",
-    "Сонік Фронтірс",
-    "Хорайзон",
-    "Чурка",
-    "Вольфенштейн"
-]
+    "бобер",
+    "кросівок",
+    "фар край",
+    "гта",
+    "бігмак",
+    "рукав",
+    "метал гір",
+    "піцерія",
+    "сонік фронтірс",
+    "хорайзон",
+    "чурка",
+    "вольфенштейн"
+];
+
+let was = []
+
+let rebus = 0;
 
 $(".slideRules").on("click",function(){
     $(".rules").slideToggle()
+})
+
+function rnd(){
+    return Math.floor(1 + Math.random()*12);
+}
+
+function startRebus(num){
+    $(".img img").attr("src",`rebuses/${num}.png`)
+    rebus = num;
+}
+
+startRebus(rnd())
+
+$("#t1btn").on("click",function(){
+    let ans = $("#t1input").val().toLowerCase()
+    if(ans == answers[rebus -  1]){
+        alertify.success("Correct!")
+        score++;
+        $("#t1input").val("")
+        $("#progress").val(score).trigger("change")
+        was.push(rebus)
+        if(score < 5) {
+        do{
+            rebus = rnd()
+
+        }while(was.includes(rebus))
+        startRebus(rebus)
+    }else{
+        $(".nextTask").css("display","block")
+    }
+} else {
+        alertify.error("Wrong!Try again!");
+    }
+})
+
+
+$("#progress").knob({
+    min: 0,
+    max: 5,
+    angleArc:120,
+    angleOffset: -60,
+    displayInput:false,
+    lineCap:"round",
+    readOnly:true
+    
 })
