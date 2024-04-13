e912951d03fdf73490522e9ede7f7122eff17735
Teremki16 <teremki16robocode@gmail.com>
2024-04-13 08:46:23 +0300

    Update at 13.04.2024

--- ND14 Web Junior/kalashnikov/quiz/task1.js
+++ ND14 Web Junior/kalashnikov/quiz/task1.js
@@ -1,6 +1,6 @@
-$(".rules").slideUp()
+$(".rules").slideUp(0)
 
-let answer = [
+let answers = [
     "дом",
     "книга",
     "баскетбол",
@@ -10,13 +10,60 @@ let answer = [
     "Вієта",
     "Проектор",
     "собака",
-    "гаманець",
+    "гаманець"
 ]
 
+let was = []
 
+let rebus = 0;
 
+let score = 0
 
 
 $(".slideRules").on("click", function(){
     $(".rules").slideToggle()
 })
+
+
+function rnd(){
+    return Math.floor(1+ Math.random() * 10)
+}
+
+function startRebus(num){
+    $(".img img").attr("src", `rebuses/${num}.png`)
+    rebus = num;
+}
+
+startRebus(rnd())
+
+$("#t1btn").on("click", function(){
+    let ans = $("#t1input").val().toLowerCase()
+    if(ans == answers[rebus - 1]){
+        alertify.success("Correct!")
+        score++;
+        $("#t1input").val()
+        $("#progress").val(score).trigger("change")
+        was.push(rebus)
+        if(score < 5){
+        do{
+            rebus = rnd()
+        }while(was.includes(rebus))
+        startRebus(rebus)
+    }else{
+        $(".nextTask").css("display", "block")
+    }
+}else{
+        alertify.error("Wrong! Try again!" )
+    }
+})
+
+$("#progress").knob({
+    min: 0,
+    max: 5,
+    angleArc: 120,
+    angleOffset: -60,
+    displayInput: false,
+    lineCap: "round",
+    readOnly:true
+})
+
