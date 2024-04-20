06b9dc2156e8f5011614ca2a3337e15addada8bf
Teremki16 <teremki16robocode@gmail.com>
2024-04-20 08:26:02 +0300

    Update at 20.04.2024

--- ND14 Web Junior/Starodubova/Quiz/task1.js
+++ ND14 Web Junior/Starodubova/Quiz/task1.js
@@ -1,19 +1,68 @@
-$(".Rules").slideUp()
+$(".rules").slideUp(0);
 
 let answers = [
-    "щур",
-    "собака",
-    "канкатинація",
-    "Іспанія",
-    "формула",
-    "телефон",
-    "фортепіано",
-    "пацюк",
-    "окуляри",
-    "стяг",
-]
+    "яблоко",
+    "груша",
+    "город",
+    "школа",
+    "сайт",
+    "браузер",
+    "плагін",
+    "колір",
+    "стиль",
+    "язик",
+    "візерунок",
+    "сорока",
+];
 
-$(".slideRules").on("click", function(){
-    $(".Rules").slideToggle()
-})
+let was = [];
 
+let rebus = 0;
+
+let score = 0;
+
+$(".slideRules").on("click", function () {
+    $(".rules").slideToggle();
+});
+
+function rnd() {
+    return Math.floor(1 + Math.random() * 12);
+}
+
+function startRebus(num) {
+    $(".img img").attr("src", `rebuses/${num}.jpg`);
+    rebus = num;
+}
+
+startRebus(rnd());
+
+$("#t1btn").on("click", function () {
+    let ans = $("#t1input").val().toLowerCase();
+    if (ans == answers[rebus - 1]) {
+        alertify.success("Correct!");
+        score++;
+        $("#t1input").val("");
+        $("#progress").val(score).trigger("change");
+        was.push(rebus);
+        if (score < 5) {
+            do {
+                rebus = rnd();
+            } while (was.includes(rebus));
+            startRebus(rebus);
+        }else{
+            $(".nextTask").css("display", "block")
+        }
+    } else {
+        alertify.error("Wrong! Try again!");
+    }
+});
+
+$("#progress").knob({
+    min: 0,
+    max: 5,
+    angleArc: 120,
+    angleOffset: -60,
+    displayInput: false,
+    lineCap: "round",
+    readOnly: true,
+});
---
+++ ND14 Web Junior/Starodubova/quiz 2/task1.js
@@ -0,0 +1,82 @@
+$(".rules").slideUp(0);
+
+let answers = [
+    ["гаррі поттер", "harry potter", "гари потер"],
+    ["спандж боб", "sponge bob", "губка боб"],
+    ["пірати карибського моря", "пірати", " капітан джек горобець", "pirates", "pirates of karibian sea"],
+    ["simpsons", "сімсони"],
+    ["зоряні війни", "я твій батько", "star wars"],
+    ["lion king", "король лев", "сімба"],
+    ["холодне серце", "frozen"],
+    ["шрек", "shrek"],
+    ["шрек", "shrek"],
+    ["роккі","rokki"]
+    ["індіана джонс", "indiana jones"],
+    ["home alone", "один вдома", "один у дома"],
+    ["термінатор", "terminator"],
+    ["back to future", "назад у майбутнє"],
+    ["мисливці на привидів", "ghost busters"],
+];
+
+let was = [];
+
+
+
+let score = 0;
+
+let time = 300
+if(localStorage.getItem("time")!= null){
+    time = localStorage.getItem("time")
+}else{
+    localStorage.setItem("time", time)
+}
+
+$(".slideRules").on("click", function () {
+    $(".rules").slideToggle();
+});
+
+function rnd() {
+    return Math.floor(1 + Math.random() * 12);
+}
+
+
+
+$("#progress").knob({
+    min: 0,
+    max: 10,
+    angleArc: 120,
+    angleOffset: -60,
+    displayInput: false,
+    lineCap: "round",
+    readOnly: true,
+});
+
+$("#time").knob({
+    min: 0,
+    max: 300,
+    displayInput: true,
+    readOnly: true,
+    width: 150,
+});
+
+function startTime(){
+    setInterval(function(){
+        time = parseInt(localStorage.getItem("time"))
+        time--;
+        $("#time").val(time).trigger("change")
+        if(time <= 0){
+            alertify.error("Ти все проспав")
+            setTimeout(()=>window.open("../quiz/task1.html"))
+            localStorage.removeItem("time")
+        }else{
+
+        localStorage.setItem("time", time)
+        }
+    }, 1000)
+}
+
+$("#start").on("click", ()=>{
+    startTime()
+    $("#start").css("display", "none")
+    $("#audio").css("display", "block")
+})
