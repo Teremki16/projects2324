be599b10f0894eb6e8eeefbda8f20447c1b7a5b1
Teremki16 <teremki16robocode@gmail.com>
2024-04-21 09:05:49 +0300

    Update at 21.04.2024

--- SB16 Web Junior/Sidlyarenko/quest 2/script.js
+++ SB16 Web Junior/Sidlyarenko/quest 2/script.js
@@ -4,17 +4,32 @@ $(".slideRules").on("click", function(){
 $(".rules").slideUp(0)
 
 let answers = [
-    "скрипт",
-    "сайт",
-    "програміст",
-    "микита",
-    "україна",
-    "математика",
-    "відеокарта",
-    "функція",
-    "мова",
-    "шо, думав в казку потрапив??"
+    ["гаррі поттер", "harry potter"],
+    ["губка боб", "sponge bob"],
+    ["пірати карибського моря", "pirates of caribean"],
+    ["simpsons", "сімсони"],
+    ["зоряні вінйи", "star wars"],
+    ["король лев", "king lion"],
+    ["холодне серце", "frozen heart"],
+    ["шрек", "shrek"],
+    ["шрек", "shrek"],
+    ["rocky", "рокі"],
+    ["ідіана джонс", "idiana jones"],
+    ["один в дома", "home alone"],
+    ["термінатор", "termit"],
+    ["назад в майбутнє", "back to the future"],
+    ["мисливці за привидами", "ghost busters"],
 ]
+
+let ts = localStorage
+let time
+
+if(ts.getItem("time") != null){
+    time = parseInt(ts.getItem("time"))
+}else{
+    time = 300
+    ts.setItem("time", 300)
+}
 
 let questionNumber = 0
 
@@ -22,47 +37,64 @@ let score = 0
 
 let was = []
 
-function rnd(){
-    return Math.floor(1 + Math.random()*10)
-}
+$("#task1btn").on("click", task)
 
-function startRebus(num){
-    $(".img img").attr("src", `rebuses/${num}.png`)
-    questionNumber = num
-}
 
-startRebus(rnd())
-
-$("#task1btn").on("click", function(){
-    if($("#task1input").val().toLowerCase() == answers[questionNumber - 1]){
-        $("#task1input").val("")
-        alertify.success("Correct!")
-        score++
-        $("#score").val(score).trigger("change")
-        was.push(questionNumber)
-        if(score < 5){
-        do{
-            questionNumber = rnd()
-        }while(was.includes(questionNumber))
-        startRebus(questionNumber)
-    }else{
-        $("#next").css("display", "block")
-        $(".img, .answer").css("display", "none")
+function task(){
+    let answer = $("#task1input").val().toLowerCase()
+    if(answers[questionNumber - 1].indexOf(answer) != -1){
+        alertify.success("correct!")
+        startQuiz(rnd())
+    } else{
+        alertify.error("wrong :(")
     }
-    }else{
-            alertify.error("Wrong! Try again")
-        }
-        
-        })
+}
+function rnd(){
+    return Math.floor(1 + Math.random()*answers.length)
+}
 
-$("#score").knob({
+$("#task1btn").on("click", task)
+   
+$("#timer").knob({
     min: 0,
-    max: 5,
-    angleArc: 120,
-    angleOffset: -60,
-    dislpayInput: false,
-    lineCap: "round",
+    max: 300,
+    readOnly: true  , //блокуємо таск
+    bgColor: "gray",
+    fgColor:"green"
+})
+$(".taskProgress").knob({
+    min: 0,
+    max: 300,
+    angularArc: 60,
+    
     readOnly: true, //блокуємо таск
     bgColor: "gray",
     fgColor:"green"
 })
+
+function startTimer(){
+    setInterval(()=>{
+        time = parseInt(ts.getItem("time")) - 1;
+        $("#timer").val(time).trigger("change")
+        if( time <=0){
+            alertify.error("time is out!")
+            setTimeout(()=> window.open("index.html", "_self", false), 1000)
+            ts.removeItem(time)
+        } else if(time > 0){
+        ts.setItem("time", time) 
+        }
+    }, 1000)
+}
+
+
+$("#start").on("click", ()=>{
+    $("#start").css("display", "none")
+    $(".sound").css("display", "block")
+    startTimer()
+    startQuiz(rnd)
+})
+
+function startQuiz(number){
+    $("audio").attr("src", `sound/${number}.mp3`)
+    questionNumber = number
+}
