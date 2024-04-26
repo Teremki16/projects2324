9755e0f00c36abf37c795ed7f8604c207c1da543
Teremki16 <teremki16robocode@gmail.com>
2024-04-26 11:03:48 +0300

    Update at 26.04.2024

--- ND14 Web Junior/Сеничак/quiz2/task1.js
+++ ND14 Web Junior/Сеничак/quiz2/task1.js
@@ -59,55 +59,54 @@ function startTime() {
     setInterval(function () {
         time = parseInt(localStorage.getItem("time"));
         time--;
-        $("#time").val(time).trigger("change")
+        $("#time").val(time).trigger("change");
         if (time <= 0) {
             alertify.error("Time is over!");
             setTimeout(() => window.open("../quiz/task1.html"));
             localStorage.removeItem("time");
-        } else if(time > 0) {
+        } else if (time > 0) {
             localStorage.setItem("time", time);
         }
     }, 1000);
 }
 
-$("#start").on("click", ()=>{
-    startTime()
-    $("#start").css("display", "none")
-    $("#audio").css("display", "block")
-    startQuest(rnd())
-})
+$("#start").on("click", () => {
+    startTime();
+    $("#start").css("display", "none");
+    $("#audio").css("display", "block");
+    startQuest(rnd());
+});
 
-function startQuest(number){
-    $("audio").attr("src", `sound/${number}.mp3`)
-    question = number
+function startQuest(number) {
+    $("audio").attr("src", `sound/${number}.mp3`);
+    question = number;
 }
 
-
-function quest(){
+function quest() {
     let answer = $("#t1input").val().toLowerCase();
-    $("#t1input").val("")
-    if(answers[question - 1].indexOf(answer) != -1){
-        alertify.success("Correct!")
+    $("#t1input").val("");
+    if (answers[question - 1].indexOf(answer) != -1) {
+        alertify.success("Correct!");
         score++;
-        $("#progress").val(score).trigger("change")
-        was.push(question)
-        if(score >= 10){
-            $("audio").css("display", "none")
-            $(".nextTask").css("display", "block")
-            localStorage.removeItem("time")
+        $("#progress").val(score).trigger("change");
+        was.push(question);
+        if (score >= 10) {
+            $("audio").css("display", "none");
+            $(".nextTask").css("display", "block");
+            localStorage.removeItem("time");
+        } else {
+            do {
+                question = rnd();
+            } while (was.includes(question));
+            startQuest(question);
         }
-        do{
-            question = rnd()
-        }while(was.includes(question))
-        startQuest(question)
-    }
-    else {
-        alertify.error("Wrong!")
+    } else {
+        alertify.error("Wrong!");
     }
 }
 
-$("#t1btn").on("click", quest)
+$("#t1btn").on("click", quest);
 
-$(document).on("keypress", (e)=>{
-    if(e.which == 13) quest();
-})
+$(document).on("keypress", (e) => {
+    if (e.which == 13) quest();
+});
