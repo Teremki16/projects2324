080b3d69c504c0d7f126660bde451dfb96ce23b2
Teremki16 <teremki16robocode@gmail.com>
2024-04-27 09:06:01 +0300

    Update at 27.04.2024

--- ND14 Web Junior/Starodubova/quiz 2/task1.js
+++ ND14 Web Junior/Starodubova/quiz 2/task1.js
@@ -1,16 +1,16 @@
 $(".rules").slideUp(0);
 
 let answers = [
-    ["гаррі поттер", "harry potter", "гари потер"],
+    ["гаррі поттер", "harry potter", "гарі потер", "хлопчик в окулярах"],
     ["спандж боб", "sponge bob", "губка боб"],
-    ["пірати карибського моря", "пірати", " капітан джек горобець", "pirates", "pirates of karibian sea"],
+    ["пірати карибського моря", "пірати", " капітан джек горобець", "pirates", "pirates of karibian sea", "капітан джек спароу"],
     ["simpsons", "сімсони"],
     ["зоряні війни", "я твій батько", "star wars"],
     ["lion king", "король лев", "сімба"],
-    ["холодне серце", "frozen"],
+    ["холодне серце", "frozen", "ельза"],
     ["шрек", "shrek"],
     ["шрек", "shrek"],
-    ["роккі","rokki"]
+    ["роккі", "rokki"],
     ["індіана джонс", "indiana jones"],
     ["home alone", "один вдома", "один у дома"],
     ["термінатор", "terminator"],
@@ -25,10 +25,11 @@ let was = [];
 let score = 0;
 
 let time = 300
-if(localStorage.getItem("time")!= null){
-    time = localStorage.getItem("time")
-}else{
-    localStorage.setItem("time", time)
+let question;
+if (localStorage.getItem("time") != null) {
+    time = parseInt(localStorage.getItem("time"));
+} else {
+    localStorage.setItem("time", 300)
 }
 
 $(".slideRules").on("click", function () {
@@ -36,7 +37,7 @@ $(".slideRules").on("click", function () {
 });
 
 function rnd() {
-    return Math.floor(1 + Math.random() * 12);
+    return Math.floor(1 + Math.random() * answers.length);
 }
 
 
@@ -59,24 +60,61 @@ $("#time").knob({
     width: 150,
 });
 
-function startTime(){
-    setInterval(function(){
+function startTime() {
+    setInterval(function () {
         time = parseInt(localStorage.getItem("time"))
         time--;
         $("#time").val(time).trigger("change")
-        if(time <= 0){
+        if (time <= 0) {
             alertify.error("Ти все проспав")
-            setTimeout(()=>window.open("../quiz/task1.html"))
+            setTimeout(() => window.open("../quiz/task1.html"))
             localStorage.removeItem("time")
-        }else{
+        } else if(time>0) {
 
-        localStorage.setItem("time", time)
+            localStorage.setItem("time", time)
         }
     }, 1000)
 }
 
-$("#start").on("click", ()=>{
+$("#start").on("click", () => {
     startTime()
     $("#start").css("display", "none")
     $("#audio").css("display", "block")
+    startQuest(rnd())
+})
+
+function startQuest(number) {
+    $("audio").attr("src", `sound/${number}.mp3`)
+    question = number
+    console.log(answers[number - 1])
+}
+
+function Quest() {
+    let answer = $("#t1input").val().toLowerCase()
+    $("#t1input").val("")
+    if (answers[question - 1].indexOf(answer) != -1) {
+        alertify.success("Correct!")
+        score++
+        $("#progress").val(score).trigger("change")
+        was.push(question)
+        if(score>= 10){
+            $("audio").css("display", "none")
+            $(".nextTask").css("display", "block")
+            localStorage.removeItem("time")
+        }else{
+            do{
+question = rnd()
+        }while(was.includes(question))
+        startQuest(question)
+        }
+        
+    } else {
+        alertify.error("Wrong!")
+    }
+}
+
+$("#t1btn").on("click", Quest)
+
+$(document).on("keypress", (e) => {
+    if (e.which == 13) Quest;
 })
