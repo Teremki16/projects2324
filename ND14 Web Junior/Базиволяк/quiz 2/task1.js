4390f4a2608fc6bf4f91ee55b5b4fabc1fc5c8ac
Teremki16 <teremki16robocode@gmail.com>
2024-04-27 09:08:35 +0300

    Update at 27.04.2024

--- ND14 Web Junior/Базиволяк/quiz 2/task1.js
+++ ND14 Web Junior/Базиволяк/quiz 2/task1.js
@@ -1,80 +1,115 @@
 $(".rules").slideUp(0)
 
-$(".slideRules").on("click", function(){
+$(".slideRules").on("click", function () {
     $(".rules").slideToggle()
 })
-let answer=[
-    ["гарі потер","harry potter"],
-    ["губка боб","sponge bob"],
-    ["пірати карибського моря","harry potter"],
-    ["ciмсои","simpson"],
-    ["зоряні війни","star wars"],
-    ["король лев","lion king"],
-    ["холодне серце","frozen"],
-    ["шрек","shrek"],
-    ["шрек","shrek"],
-    ["рокі","rocky"],
-    ["індіано джонс","indiana jones"],
-    ["один дома","один в дома","один y дома"],
-    ["термінатор","terminator"],
-    ["назад в майбутьне","back to the future"],
-    ["мисливці за привидами","ghost busters"],
+let answers = [
+    ["гарі потер", "harry potter"],
+    ["губка боб", "sponge bob"],
+    ["пірати карибського моря", "harry potter"],
+    ["ciмсои", "simpson"],
+    ["зоряні війни", "star wars"],
+    ["король лев", "lion king"],
+    ["холодне серце", "frozen"],
+    ["шрек", "shrek"],
+    ["шрек", "shrek"],
+    ["рокі", "rocky"],
+    ["індіано джонс", "indiana jones"],
+    ["один дома", "один в дома", "один y дома"],
+    ["термінатор", "terminator"],
+    ["назад в майбутьне", "back to the future"],
+    ["мисливці за привидами", "ghost busters"],
 ];
 
-let was =[]
+let was = []
 
 let score = 0
 
 let time = 300;
-if(localStorage.getItem("time") != null){
-    time = localStorage.getItem("time")
-}else{
-    localStorage.setItem("time", 300)
+let question = 0;
+if (localStorage.getItem("time") != null) {
+    time = parseInt(localStorage.getItem("time"))
+} else {
+    localStorage.setItem("time", 300);
 }
 
-
-
-
-
+function rnd() {
+    return Math.floor(1 + Math.random() * answers.length)
+}
 
 $("#progress").knob({
- min: 0,
- max:5,
- angleArc:120,
- angleOffset: -60,
-displayInput: false ,
-lineCap: "round",
-readOnly: true
+    min: 0,
+    max: 5,
+    angleArc: 120,
+    angleOffset: -60,
+    displayInput: false,
+    lineCap: "round",
+    readOnly: true
 })
 
 $("#time").knob({
- min: 0,
- max: 300,
-displayInput: true,
-readOnly: true,
+    min: 0,
+    max: 300,
+    displayInput: true,
+    readOnly: true,
 })
 
 
-function startTime (){
-    setInterval(function(){
+function startTime() {
+    setInterval(function () {
         time = parseInt(localStorage.getItem("time"))
         time--;
         $("#time").val(time).trigger("change")
-        if(time <= 0){
+        if (time <= 0) {
             alertify.error("Time is over")
-            setTimeout(()=>window.open("../quiz/task1.html")) ;
-                localStorage.removeItem("time")
-            }else{
-                localStorage.setItem("time", time)
-            }
+            setTimeout(() => window.open("../quiz/task1.html"));
+            localStorage.removeItem("time")
+        } else if(time>0){
+            localStorage.setItem("time", time)
+        }
         
      
     }, 1000)
 }
 
-$("#start").on("click",()=>{
-startTime()
-$("#start").css("display", "none")
-$("#audio").css("display", "block")
+$("#start").on("click", () => {
+    startTime()
+    $("#start").css("display", "none")
+    $("#audio").css("display", "block")
+    startQuest(rnd());
+})
+
+function startQuest(number) {
+    $("audio").attr("src", `sound/${number}.mp3`)
+    question = number
 }
-)
+
+function quest() {
+    let answer = $("#t1input").val().toLowerCase()
+    $("#t1input").val("")
+    if (answers[question - 1].indexOf(answer) != -1) {
+        alertify.success("Correct!")
+        score++;
+        $("#progress").val(score).trigger("change")
+        was.push(question)
+        if (score >= 10) {
+            $("audio").css("display", "none")
+            $(".nextTask").css("display", "block")
+            localStorage.removeItem("time")
+        } else {
+            do {
+                question = rnd()
+            } while (was.includes(question))
+            startQuest(rnd())
+        }
+
+    } else {
+        alertify.error("Wrong!")
+    }
+}
+
+$("#t1btn").on("click", quest)
+
+$(document).on("keypress", (e) => {
+    if (e.which == 13) quest();
+})
