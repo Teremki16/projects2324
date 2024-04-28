edecb5a8cd535540cd42fbba5cd7671bac65b6e4
Teremki16 <teremki16robocode@gmail.com>
2024-04-28 08:22:32 +0300

    Update at 28.04.2024

---
+++ SB10 Middle embedded/Tetris_Leshchenko/Tetris_Leshchenko.ino
@@ -0,0 +1,27 @@
+#include <GameBoy.h>
+#include "Blocks.h"
+GameBoy gb;
+
+void setup() {
+  gb.begin(8);
+
+}
+void loop() {
+gb.clearDisplay();
+drawBlock(I_block_1, 3, 3);
+delay(100);
+
+gb.clearDisplay();
+drawBlock(L_block_1, 3, 3);
+delay(100);
+}
+
+void drawBlock(byte arr[4][4], int x, int y) {
+  for (int i = 0;  i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      if (arr[j][i] == 1) {
+        gb.drawPoint(x + i, y + j);
+      }
+    }
+  }
+}
