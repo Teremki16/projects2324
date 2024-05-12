b5508cc52292872b225f8168d0859b376ad39a6d
Teremki16 <teremki16robocode@gmail.com>
2024-05-12 08:48:13 +0300

    Update at 12.05.2024

--- SB10 Middle embedded/TETRIS_MIKHEEVA_LIZA_10/TETRIS_MIKHEEVA_LIZA_10.ino
+++ SB10 Middle embedded/TETRIS_MIKHEEVA_LIZA_10/TETRIS_MIKHEEVA_LIZA_10.ino
@@ -3,15 +3,23 @@
 GameBoy gb;
 
 int x = 2, y = -1;
+int rot = 0;
 
 void setup() {
   gb.begin(8);
+  randomSeed(analogRead(0) + analogRead(5));
+  CreateBlock(random(7));
 }
 
 void loop() {
   makeMove();
-  drawBlock(S_Block_1, x, y);
-  y++;
+  if (gb.checkBlockCollision(gb.block[rot], x,  y)) {
+  gb.memBlock(gb.block[rot], x, y);
+  }else{
+    y++;
+  }
+  gb.drawDisplay();
+  drawBlock(gb.block[rot], x, y);
   delay(100);
 
 }
@@ -27,21 +35,22 @@ void drawBlock(byte arr [4][4], int x, int y) {
 }
 
 void makeMove() {
-  if (gb.getKey() == 4) {
-    x--;
-  }
-  if (gb.getKey() == 5) {
-    x++;
-  }
+  if (gb.getKey() == 4  && gb.checkBlockCollision(gb.block[rot], x - 1, y)) {
+  x--;
+}
+if (gb.getKey() == 5 && gb.checkBlockCollision(gb.block[rot], x + 1, y)) {
+  x++;
+}
 }
 
 void CreateBlock(int num) {
-  if (num == 0) gb.generateBlock(gb.block, I_block_1, I_block_2, I_block_3, I_block_4);
-  if (num == 1) gb.generateBlock(gb.block, Z_block_1, Z_block_2, Z_block_3, Z_block_4);
-  if (num == 2) gb.generateBlock(gb.block, S_block_1, S_block_2, S_block_3, S_block_4);
-  if (num == 3) gb.generateBlock(gb.block, L_block_1, L_block_2, L_block_3, L_block_4);
-  if (num == 4) gb.generateBlock(gb.block, J_block_1, J_block_2, J_block_3, J_block_4);
-  if (num == 5) gb.generateBlock(gb.block, T_block_1, T_block_2, T_block_3, T_block_4);
-  if (num == 6) gb.generateBlock(gb.block, 0_block_1, 0_block_2, 0_block_3, 0_block_4);
+  x = 2, y = -1, rot = random(0, 4);
+  if (num == 0) gb.generateBlock(gb.block, I_Block_1, I_Block_2, I_Block_3, I_Block_4);
+  if (num == 1) gb.generateBlock(gb.block, Z_Block_1, Z_Block_2, Z_Block_3, Z_Block_4);
+  if (num == 2) gb.generateBlock(gb.block, S_Block_1, S_Block_2, S_Block_3, S_Block_4);
+  if (num == 3) gb.generateBlock(gb.block, L_Block_1, L_Block_2, L_Block_3, L_Block_4);
+  if (num == 4) gb.generateBlock(gb.block, J_Block_1, J_Block_2, J_Block_3, J_Block_4);
+  if (num == 5) gb.generateBlock(gb.block, T_Block_1, T_Block_2, T_Block_3, T_Block_4);
+  if (num == 6) gb.generateBlock(gb.block, O_Block_1, O_Block_2, O_Block_3, O_Block_4);
 
 }
