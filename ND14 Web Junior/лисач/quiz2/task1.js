9d16ea044248f16f88cd56c52f86efe0b323954a
Teremki16 <teremki16robocode@gmail.com>
2024-05-12 08:52:56 +0300

    Update at 12.05.2024

--- ND14 Web Junior/лисач/quiz2/task1.js
+++ ND14 Web Junior/лисач/quiz2/task1.js
@@ -1,21 +1,66 @@
-$(".rules").slideUp(0)
+$(".rules").slideUp(0);
 
-let answers = [
-    ["гаррі поттер", "harry poter", "harry", "potter"],
-    ["губка боб", "sponge bob", "sponge", "губка", "bob"],
-    ["джек воробей", "пірати"],
-    ["сімпсониб", "симсоны", "simpson"],
-    ["зоряні війни","star wars"],
-    ["король лев","lion king"],
-    ["холодне серце","frozen"],
-    ["шрек","shrek"],
-    ["шрек","shrek"],
-    ["роккі","rocky"],
-    ["індіана джонс","indiana jones"],
-    ["один вдома","home alone"],
-    ["термінатор","terminator"],
-    ["назад у майбутнє","back to the future"],
-    ["мисливці на привдів","ghost busters"],
+let cards = [
+    {
+        name: "сэнди",
+        img: "https://i.pinimg.com/564x/f4/43/d8/f443d890b41f6d8c8475c895a598337c.jpg",
+        id:1 
+    },
+    {
+        name: "спайк",
+        img: "https://pikuco.ru/upload/test_stable/abf/abfbec18c2157cb923bd57d194d0cbb9.webp",
+        id: 2
+    },
+    {
+        name: "диномайк",
+        img: "https://pm1.aminoapps.com/7539/7030adc44ee97efa32a3ae33d9da1521f491abedr1-767-804v2_uhq.jpg",
+        id: 3
+    },
+    {
+        name: "лола",
+        img: "https://avatars.dzeninfra.ru/get-zen_doc/1579004/pub_61b9af238d149b62f602ea19_61b9bdf873fbf737dd7fe18f/scale_1200",
+        id: 4
+    },
+    {
+        name: "манда",
+        img: "https://pikuco.ru/upload/test_stable/8c8/8c8d28743815885c8ccdeb5da53a8e4e.webp",
+        id: 5
+    },
+    {
+        name: "мэлоди",
+        img: "https://static.wikia.nocookie.net/brawlstars/images/b/b1/%D0%9C%D0%B5%D0%BB%D0%BE%D0%B4%D0%B8_Skin-Default.png/revision/latest/smart/width/250/height/250?cb=20240302105252&path-prefix=ru",
+        id: 6
+    },
+    {
+        name: "кольт",
+        img: "https://kartinki.pics/uploads/posts/2022-03/1646140772_3-kartinkin-net-p-kartinki-bravlerov-iz-bravl-stars-4.jpg",
+        id: 7
+    },
+    {
+        name: "чарли",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2uMwOctJ-xaQMk7wLV2rQe2bpJPh7oYaiaTUdDx-KPA&s",
+        id: 8
+    },
+    {
+        name: "пайпер",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvHiOjD6mOGSkW-EelNrLTFA7ZXYmjp25of2HDqygofw&s",
+        id: 9
+    },
+    {
+        name: "честер",
+        img: "https://static.wikia.nocookie.net/brawlstars/images/9/90/%D0%A7%D0%B5%D1%81%D1%82%D0%B5%D1%80_Skin-Default.png/revision/latest/smart/width/250/height/250?cb=20221218183256&path-prefix=ru",
+        id: 10
+    },
+    {
+        name: "белль",
+        img: "https://static.wikia.nocookie.net/brawlstars/images/8/81/%D0%AF%D0%B4%D0%BE%D0%B2%D0%B8%D1%82%D1%8B%D0%B9_%D0%BF%D0%BB%D1%8E%D1%89_%D0%91%D0%B5%D0%BB%D0%BB%D1%8C.png/revision/latest/smart/width/300/height/300?cb=20220303135217&path-prefix=ru",
+        id: 11
+    },
+    {
+        name: "фэнг",
+        img: "https://i.pinimg.com/736x/11/3a/07/113a076143cac538f717f1dc2b4b29ec.jpg",
+        id: 12
+    },
 ]
 
 let was = []
@@ -35,7 +80,7 @@ $(".slideRules").on("click", function(){
 })
 
 function rnd(){
-    return Math.floor(1+Math.random() * answers.length);
+    return Math.floor(Math.random() * 12);
 }
 
 $("#progress").knob({
@@ -75,40 +120,5 @@ function startTime(){
 $("#start").on("click", ()=>{
     startTime()
     $("#start").css("display", "none")
-    $("#audio").css("display", "block")
-    startQuest(rnd())
-})
-
-function startQuest(number){
-    $("audio").attr("src", `sound/${number}.mp3`)
-    question = number
-}
-
-function quest(){
-    let answer = $("#t1input").val().toLowerCase();
-    $("#t1input").val("")
-    if(answers[question - 1].indexOf(answer) != -1){
-        alertify.success("Correct!")
-        score++;
-        $("#progress").val(score).trigger("change")
-        was.push(question)
-        if(score > 1){
-            $("audio").css("display", "none")
-            $("nextTask").css("display", "block")
-            localStorage.removeItem("time")
-        } else {
-            do{
-                question = rnd()
-            }while(was.includes(question))
-            startQuest(question)
-        }
-    } else {
-        alertify.error("wrong!")
-    }
-}
-
-$("#t1btn").on("click", quest)
-
-$(document).on("keypress", (e)=>{
-    if(e.which == 13) quest();
+   
 })
