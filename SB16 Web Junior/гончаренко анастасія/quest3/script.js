5c6215cea9fa192160cda736db46fcbeef3c2eea
Teremki16 <teremki16robocode@gmail.com>
2024-05-12 08:52:54 +0300

    Update at 12.05.2024

--- SB16 Web Junior/гончаренко анастасія/quest3/script.js
+++ SB16 Web Junior/гончаренко анастасія/quest3/script.js
@@ -3,81 +3,82 @@ $(".slideRules").on("click", function () {
 })
 $(".rules").slideUp(0);
 
-let cards =[
+let cards = [
     {
         name: "папугай кеша",
-        img:"https://avatars.dzeninfra.ru/get-zen_doc/9369849/pub_6431a8f407490e6f8d96c569_6431bb54b628cd1a3c8d2ef2/scale_1200",
+        img: "https://avatars.dzeninfra.ru/get-zen_doc/9369849/pub_6431a8f407490e6f8d96c569_6431bb54b628cd1a3c8d2ef2/scale_1200",
         id: 1
     },
     {
         name: "кот",
-        img:"https://cdn.iz.ru/sites/default/files/news-2024-01/1_4_4.jpg",
+        img: "https://cdn.iz.ru/sites/default/files/news-2024-01/1_4_4.jpg",
         id: 2
-        
+
     },
     {
         name: "index.html",
-        img:"https://itproger.com/img/courses/1476977240.jpg",
+        img: "https://itproger.com/img/courses/1476977240.jpg",
         id: 3
 
     },
     {
         name: "банан",
-        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr1r45-I-xegqHULouMgBcskyifFZoHbuK5sFdhaYUHg&s",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr1r45-I-xegqHULouMgBcskyifFZoHbuK5sFdhaYUHg&s",
         id: 4
 
     },
     {
         name: "script",
-        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png",
         id: 5
 
     },
     {
         name: "серце",
-        img:"https://www.liga.net/images/general/2020/09/29/20200929173341-6915.jpg?v=1601394055",
+        img: "https://www.liga.net/images/general/2020/09/29/20200929173341-6915.jpg?v=1601394055",
         id: 6
 
     },
     {
         name: "собака",
-        img:"https://rg.ru/uploads/images/214/34/08/photorep_imageid_538385_8085b70e8b6927e1575618884.jpg",
+        img: "https://rg.ru/uploads/images/214/34/08/photorep_imageid_538385_8085b70e8b6927e1575618884.jpg",
         id: 7
 
     },
     {
         name: "хом'як",
-        img:"https://tierarzt-karlsruhe-durlach.de/storage/2023/07/hamster-1555083.jpg",
+        img: "https://tierarzt-karlsruhe-durlach.de/storage/2023/07/hamster-1555083.jpg",
         id: 8
 
     },
     {
         name: "пістоль",
-        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/FN_Model_1903_002_%28cropped%29.jpg/640px-FN_Model_1903_002_%28cropped%29.jpg",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/FN_Model_1903_002_%28cropped%29.jpg/640px-FN_Model_1903_002_%28cropped%29.jpg",
         id: 9
 
     },
     {
         name: "скретч",
-        img:"https://robx.org/wp-content/uploads/2020/10/scratch_logo.png",
+        img: "https://robx.org/wp-content/uploads/2020/10/scratch_logo.png",
         id: 10
 
     },
     {
         name: "змія",
-        img:"https://www.newacropolis.org.ua/uploads/production/ckeditor/picture/data/650/717/91-/65071791-ef48-4b3c-be61-da9718c3ff38/content.png",
+        img: "https://www.newacropolis.org.ua/uploads/production/ckeditor/picture/data/650/717/91-/65071791-ef48-4b3c-be61-da9718c3ff38/content.png",
         id: 11
 
     },
     {
         name: "кот2",
-        img:"https://s13.stc.yc.kpcdn.net/share/i/instagram/B44solahwlo/wr-1280.webp",
+        img: "https://s13.stc.yc.kpcdn.net/share/i/instagram/B44solahwlo/wr-1280.webp",
         id: 12
 
     },
 ]
 
-
+let firstCard = null;
+let secondCard = null;
 
 let ts = localStorage
 let time;
@@ -98,13 +99,13 @@ let score = 0;
 
 
 function rnd(n) {
-    return Math.floor( Math.random() * n);
+    return Math.floor(Math.random() * n);
 }
 
 
 $("#score").knob({
     min: 0,
-    max: 5,
+    max: 12,
     angleArc: 120,
     angleOffset: -60,
     displayInput: false,
@@ -139,9 +140,71 @@ function startTimer() {
 
 $("#start").on("click", () => {
     $("#start").css("display", "none")
+    $(".gameBoard").css("display", "grid")
     startTimer()
-
+    fillBoard()
 })
 
+function fillBoard() {
+    let board = [...cards, ...cards]
+    board = shuffle(board)
+    for (let i = 0; i < board.length; i++) {
+        let cardHtml = `
+        <div class="card" data-id="${board[i].id}">
+                    <div class="front">cards</div>
+                    <div class="back"><img src="${board[i].img}" alt="${board[i].name}"></div>
+                </div>
+        `
+        $(".gameBoard").append(cardHtml)
+    }
+}
+
+
+
+function shuffle(array) {
+    let counter = array.length
+    let temp;
+    let index;
+    while (counter > 0) {
+        index = Math.floor(Math.random() * counter)
+        counter--;
+        temp = array[counter]
+        array[counter] = array[index]
+        array[index] = temp
+    }
+    return array;
+}
+
+
+function cardClicked() {
+    if (secondCard || $(this).hasClass("matched")) return;
+    if (!firstCard) {
+        firstCard = $(this)
+        firstCard.addClass("flip")
+        return;
+    }
+    if (firstCard && !$(this).hasClass("flip")) {
+        secondCard = $(this)
+        secondCard.addClass("flip")
+        if (firstCard.attr("data-id") == secondCard.attr("data-id")) {
+            firstCard.addClass("matched")
+            secondCard.addClass("matched")
+            firstCard = null
+            secondCard = null
+            score++;
+            $("#score").val(score).trigger("change")
+        } else {
+            setTimeout(() => {
+                firstCard.removeClass("flip")
+                secondCard.removeClass("flip")
+                firstCard = null
+                secondCard = null
+
+            }, 500)
+
+        }
+    }
+}
+    $(document).on("click", ".card", cardClicked)
 
 
