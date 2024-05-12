b5c13d2598c0a7c20ee0d438e39d8455c57fc74f
Teremki16 <teremki16robocode@gmail.com>
2024-05-12 08:48:21 +0300

    Update at 12.05.2024

--- SB14 Middle embedded/tetris_migalush/tetris_migalush.ino
+++ SB14 Middle embedded/tetris_migalush/tetris_migalush.ino
@@ -1,17 +1,27 @@
 #include <GameBoy.h>
 #include "blocks.h"
 GameBoy GB;
-int n = 0;
+int x = 2,y = -1;
+int rot = 0;
 
 void setup() {
   GB.begin(8);
+  randomSeed(analogRead(0) + analogRead(5));
+  createBlock(random(7));
 
 }
 
 void loop() {
-  if(GB.getKey() == 1) n++;
+  MakeMove();
+  if(GB.checkBlockCollision(GB.block[rot],x,y)){
+   GB.memBlock(GB.block[rot],x,y);
+   createBlock(random(7));
+  }else{
+    y++;
+  }
   GB.clearDisplay();
-  if(n == 0) drawBlock(I_block_1)
+  drawBlock(GB.block[rot],x,y);
+  delay(100);
 }
 
 void drawBlock(byte arr[4][4], int x, int y) {
@@ -22,4 +32,24 @@ void drawBlock(byte arr[4][4], int x, int y) {
       }
     }
   }
+}
+
+void MakeMove(){
+  if(GB.getKey() == 4 && GB.checkBlockCollision(GB.block[rot], x - 1,y)){
+    x--;
+  }
+  if(GB.getKey() == 5 && GB.checkBlockCollision(GB.block[rot], x + 1,y)){
+    x++;
+  }
+}
+
+void createBlock(int num){
+  x = 2, y = -1, rot = random(0,4);
+  if(num == 0) GB.generateBlock(GB.block, I_block_1,I_block_2,I_block_3,I_block_4);
+  if(num == 1) GB.generateBlock(GB.block, Z_block_1,Z_block_2,Z_block_3,Z_block_4);
+  if(num == 2) GB.generateBlock(GB.block, S_block_1,S_block_2,S_block_3,S_block_4);
+  if(num == 3) GB.generateBlock(GB.block, L_block_1,I_block_2,L_block_3,L_block_4);
+  if(num == 4) GB.generateBlock(GB.block, J_block_1,J_block_2,J_block_3,J_block_4);
+  if(num == 5) GB.generateBlock(GB.block, T_block_1,T_block_2,T_block_3,T_block_4);
+  if(num == 6) GB.generateBlock(GB.block, O_block_1,O_block_2,O_block_3,O_block_4);
 }
--- SB16 Web Junior/AkimovaYuliaWeb/MySelf3/script.js
+++ SB16 Web Junior/AkimovaYuliaWeb/MySelf3/script.js
@@ -16,28 +16,28 @@ let card=[
         id:2,
     },
     {
-        name:"",
-        img:"",
+        name:"Птичка",
+        img:"https://i.pinimg.com/564x/bc/99/f4/bc99f4c0dcd9be69fb97e91c206ba8a8.jpg",
         id:3,
     },
     {
-        name:"",
-        img:"",
+        name:"Ласка",
+        img:"https://i.pinimg.com/564x/c4/bb/31/c4bb31ba56d1bf6ba430dc892c642ef3.jpg",
         id:4,
     },
     {
-        name:"Кактус",
-        img:"https://www.anekdot.ru/i/caricatures/normal/20/9/18/1600450998.jpg",
+        name:"москсвинка",
+        img:"https://i.pinimg.com/564x/f9/ea/00/f9ea00376c104049bbd59ccc79b0f99e.jpg",
         id:5,
     },
     {
-        name:"",
-        img:"",
+        name:"ааа",
+        img:"https://i.pinimg.com/564x/2e/86/35/2e863531d01b748ed6811196e2e0d167.jpg",
         id:6,
     },
     {
-        name:"Ківі",
-        img:"https://i.pinimg.com/564x/74/b0/1e/74b01eaa9b5bd6ec68488d001e3afa70.jpg",
+        name:"Пт ківі",
+        img:"https://i.pinimg.com/564x/26/cf/6f/26cf6f341d495cfaeede7dc02a3e0428.jpg",
         id:7,
     },
     {
@@ -46,26 +46,29 @@ let card=[
         id:8,
     },
     {
-        name:"",
-        img:"",
+        name:"дракон",
+        img:"https://i.pinimg.com/564x/ef/35/3d/ef353d5040e398243f9be7d6b3ce7b7c.jpg",
         id:9,
     },
     {
-        name:"",
-        img:"",
+        name:"лягушка",
+        img:"https://i.pinimg.com/474x/f1/d7/ac/f1d7ac84cd3d712dca5278c511a663ca.jpg",
         id:10,
     },
     {
-        name:"",
-        img:"",
+        name:"коот",
+        img:"https://i.pinimg.com/564x/aa/ce/d6/aaced6fdf6514555608ff45172f514c9.jpg",
         id:11,
     },
     {
-        name:"Пітух",
-        img:"https://i.pinimg.com/474x/22/18/24/221824d83f9fd80a0f08ec0d2986870e.jpg",
+        name:"ида",
+        img:"https://i.pinimg.com/564x/a3/71/3b/a3713bfb24373b695e339d9d77f7d682.jpg",
         id:12,
     },
 ]
+
+let firstCard=null
+let secondCard=null
 
 let ts=localStorage
 let time
@@ -83,7 +86,7 @@ function rnd(n){
 
 $("#score").knob({
     min: 0,
-    max: 5,
+    max: 12,
     angleArc: 120,
     angleOffset: -60,
     displayInput: false,
@@ -101,7 +104,7 @@ $("#timer").knob({
     fgColor: "hotpink",
 })
 
-function sT(){
+function startTimer(){
     setInterval(()=>{
         time=parseInt(ts.getItem("time"))-1
         $("#timer").val(time).trigger("change")
@@ -118,11 +121,13 @@ function sT(){
 $("#start").on("click",()=>{
     $("#start").css("display","none")
     $(".gameBoard").css("display","grid")
-    sT()
+    startTimer()
+    fillBoard()
 })
 
 function fillBoard(){
     let board=[...card, ...card]
+    board=shuffle(board)
     for(let i=0;i<board.length;i++){
         let cardHtml=`
         <div class="card" data-id="${board[i].id}">
@@ -133,4 +138,52 @@ function fillBoard(){
         $(".gameBoard").append(cardHtml)
     }
 }
-fillBoard()
+
+function shuffle(array){
+    let counter=array.length
+    let temp
+    let index
+    while(counter>0){
+        index=Math.floor(Math.random()*counter)
+        counter--
+        temp=array[counter]
+        array[counter]=array[index]
+        array[index]=temp
+    }
+    return array;
+}
+
+function cardClicked(){
+    if(secondCard || $(this).hasClass("matched")){return}
+    if(!firstCard){
+        firstCard=$(this)
+        firstCard.addClass("flip")
+        return;
+    }
+    if(firstCard && !$(this).hasClass("flip")){
+        secondCard=$(this)
+        secondCard.addClass("flip")
+        if(firstCard.attr("data-id")==secondCard.attr("data-id")){
+            firstCard.addClass("matched")
+            secondCard.addClass("matched")
+            firstCard=null
+            secondCard=null
+            score++
+            $("#score").val(score).trigger("change")
+            if(score==12){
+                $("#win").css("display","flex")
+                ts.removeItem("time")
+            }
+        }
+    else{
+        setTimeout(()=>{
+            firstCard.removeClass("flip")
+            secondCard.removeClass("flip")
+            firstCard=null
+            secondCard=null
+        },500)
+    }
+}
+    
+}
+$(document).on("click", ".card",cardClicked)
