c8980023c8ca7831fd6544e3a3ce4bd51283070f
Teremki16 <teremki16robocode@gmail.com>
2024-05-19 09:05:56 +0300

    Update at 19.05.2024

--- SB10 Middle embedded/TETRIS_MIKHEEVA_LIZA_10/TETRIS_MIKHEEVA_LIZA_10.ino
+++ SB10 Middle embedded/TETRIS_MIKHEEVA_LIZA_10/TETRIS_MIKHEEVA_LIZA_10.ino
@@ -4,23 +4,61 @@ GameBoy gb;
 
 int x = 2, y = -1;
 int rot = 0;
+int fallSpeed = 100;
+int acc = 1;
+int scores = 0;
+int level = 0;
+
 
 void setup() {
   gb.begin(8);
   randomSeed(analogRead(0) + analogRead(5));
   CreateBlock(random(7));
+  Serial.begin(9600);
 }
 
 void loop() {
-  makeMove();
-  if (gb.checkBlockCollision(gb.block[rot], x,  y)) {
-  gb.memBlock(gb.block[rot], x, y);
-  }else{
-    y++;
+  if (loss()) {
+    Serial.println("you are lost the game");
+    Serial.println("Score: " + (String)scores);
+    clearMemory();
+    scores = 0;
+    gb.sound(COLLISION);
+    gb.testMatrix(10);
   }
-  gb.drawDisplay();
-  drawBlock(gb.block[rot], x, y);
-  delay(100);
+  if (win()) {
+    clearMemory();
+    scores = 0;
+    level = 0;
+    for (int i = 0; i < 8; i++) {
+      for (int j = 0; j < 16; j++){
+      gb.setLed(i, j, WIN[j][i]);
+    }
+  }
+  delay(2000);
+  gb.clearDisplay();
+}
+makeMove();
+if (gb.checkBlockCollision(gb.block[rot], x , y + 1)) {
+  gb.memBlock(gb.block[rot], x, y);
+  CreateBlock(random(7));
+
+  int lines = gb.fullLine();
+  if (lines != 0) {
+    scores += lines;
+    level += lines;
+    gb.sound(SCORE);
+    if (level >= 5) {
+      acc++;
+      level = 0;
+    }
+  }
+} else {
+  y++;
+}
+gb.drawDisplay();
+drawBlock(gb.block[rot], x, y);
+delay(fallSpeed / acc);
 
 }
 
@@ -35,12 +73,21 @@ void drawBlock(byte arr [4][4], int x, int y) {
 }
 
 void makeMove() {
-  if (gb.getKey() == 4  && gb.checkBlockCollision(gb.block[rot], x - 1, y)) {
-  x--;
-}
-if (gb.getKey() == 5 && gb.checkBlockCollision(gb.block[rot], x + 1, y)) {
-  x++;
-}
+  if (gb.getKey() == 4  && !gb.checkBlockCollision(gb.block[rot], x - 1, y)) {
+    x--;
+  }
+  if (gb.getKey() == 5 && !gb.checkBlockCollision(gb.block[rot], x + 1, y)) {
+    x++;
+  }
+  if (gb.getKey() == 1 && !gb.checkBlockCollision(gb.block[rot + 1], x, y)) {
+    rot++;
+    if (rot > 3) rot = 0;
+  }
+  if (gb.getKey() == 6) {
+    acc = 4;
+  } else {
+    acc = 1;
+  }
 }
 
 void CreateBlock(int num) {
@@ -53,4 +100,26 @@ void CreateBlock(int num) {
   if (num == 5) gb.generateBlock(gb.block, T_Block_1, T_Block_2, T_Block_3, T_Block_4);
   if (num == 6) gb.generateBlock(gb.block, O_Block_1, O_Block_2, O_Block_3, O_Block_4);
 
+}
+
+bool win() {
+  if (scores >= 20) {
+    return true;
+  }
+  return false;
+}
+
+void clearMemory() {
+  for (int i = 0; i < 16; i++) {
+    for (int j = 0; j < 8; j++) {
+      gb.wipePoint(j, i);
+
+    }
+  }
+}
+bool loss() {
+  if (gb.checkBlockCollision(gb.block[rot], x, 0)) {
+    return true;
+  }
+  return false;
 }
