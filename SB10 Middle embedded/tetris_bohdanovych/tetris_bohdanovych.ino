de30dc6304383b728f884ba28d3be9f062e51c0f
Teremki16 <teremki16robocode@gmail.com>
2024-05-19 09:12:14 +0300

    Update at 19.05.2024

--- SB10 Middle embedded/tetris_bohdanovych/tetris_bohdanovych.ino
+++ SB10 Middle embedded/tetris_bohdanovych/tetris_bohdanovych.ino
@@ -4,24 +4,60 @@ GameBoy gb;
 
 int x = 2, y = -1;
 int rot = 0;
+int fallSpeed = 150;
+int acc = 1;
+int scores = 0;
+int level = 0;
 
 void setup() {
   gb.begin(8);
   randomSeed(analogRead(0) + analogRead(5));
   createblock(random(7));
+  Serial.begin(9600);
 }
 
 void loop() {
+  if(loss()){
+    Serial.println("you are lost the game:)");
+    Serial.println("Score:" + (String)scores);
+    clearMemory();
+    scores = 0;
+    gb.sound(COLLISION);
+    gb.testMatrix(10);
+  }
+  if(win()){
+    clearMemory();
+    scores = 0;
+    level = 0;
+    for(int i = 0;i < 8; i++){
+      for(int j = 0; j < 16; j++){
+        gb.setLed(i, j,WIN[j][i]);
+        
+      }
+    }
+    delay(2000);
+    gb.clearDisplay();
+  }
   makeMove();
-  if(gb.checkBlockCollision(gb.block[rot], x ,y)){
+  if(gb.checkBlockCollision(gb.block[rot], x ,y + 1)){
   gb.memBlock(gb.block[rot], x, y);
   createblock(random(7));
+  int lines = gb.fullLine();
+  if(lines != 0){
+    level += lines;
+    scores += lines;
+    gb.sound(SCORE);
+  }
+  if(level >= 5){
+    acc++;
+    level = 0;
+  }
   }else{
   y++;
 }
   gb.drawDisplay();
   drawBlock(gb.block[rot],x,y);
-  delay(100);
+  delay(fallSpeed / acc);
 }
 void drawBlock(byte arr[4][4], int x, int y){
   for(int i = 0; i < 4; i++){
@@ -41,6 +77,15 @@ void makeMove(){
   if(gb.getKey() == 5 && !gb.checkBlockCollision(gb.block[rot], x + 1, y)){
     x++;
   }
+  if(gb.getKey() == 1 && !gb.checkBlockCollision(gb.block[rot + 1], x, y)){
+    rot++;
+    if(rot > 3) rot = 0;
+  }
+  if(gb.getKey() == 6){
+    acc = 4;
+  }else{
+    acc = 1;
+  }
 }
 
 void createblock(int num){
@@ -52,4 +97,27 @@ void createblock(int num){
   if(num ==4) gb.generateBlock(gb.block, J_block_1,J_block_2,J_block_3,J_block_4);
   if(num ==5) gb.generateBlock(gb.block, T_block_1,T_block_2,T_block_3,T_block_4);
   if(num ==6) gb.generateBlock(gb.block, O_block_1,O_block_2,O_block_3,O_block_4);
+}
+
+bool loss(){
+  if(gb.checkBlockCollision(gb.block[rot], x, 0)){
+    return true;
+  }
+  return false; 
+}
+
+bool win(){
+  if(scores >= 20){
+    return true;
+  }
+  return false; 
+}
+
+
+void clearMemory(){
+  for(int i = 0;i < 16; i++){
+    for(int j = 0;j < 8; j++){
+      gb.wipePoint(j, i);
+    }
+  }
 }
