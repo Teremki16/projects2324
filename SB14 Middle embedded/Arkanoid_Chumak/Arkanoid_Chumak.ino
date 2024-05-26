737fafc221becf10ff6101d697bf93fd87f3a367
Teremki16 <teremki16robocode@gmail.com>
2024-05-26 08:01:59 +0300

    Update at 26.05.2024

---
+++ SB14 Middle embedded/Arkanoid_Chumak/Arkanoid_Chumak.ino
@@ -0,0 +1,35 @@
+#include <GameBoy.h>
+#include "Blocks.h"
+GameBoy gb;
+
+int paddleX = 3;
+int paddleY = 14;
+
+void setup() {
+gb.begin(8);
+}
+
+void loop() {
+  makePaddle();
+  gb.drawDisplay();
+  drawPaddle(paddle, paddleX, paddleY);
+  delay(500);
+}
+
+void drawPaddle(byte arr[3], int x, int y){
+  for(int i = 0; i < 3; i++){
+    if(arr[i] == 1){
+      gb.drawPoint(x + i, y);
+    }
+  }
+}
+
+
+void makePaddle(){
+  if (gb.getKey() == 4 && paddleX > 0){
+    paddleX--;
+  }
+  if (gb.getKey() == 5 && paddleX < 5){
+    paddleX++;
+  }
+}
