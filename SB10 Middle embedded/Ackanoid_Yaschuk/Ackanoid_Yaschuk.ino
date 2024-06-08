e3f7176f7dc02571928d65756004c0aaf5d0860b
Teremki16 <teremki16robocode@gmail.com>
2024-06-08 08:21:47 +0300

    Update at 08.06.2024

--- SB10 Middle embedded/Ackanoid_Yaschuk/Ackanoid_Yaschuk.ino
+++ SB10 Middle embedded/Ackanoid_Yaschuk/Ackanoid_Yaschuk.ino
@@ -5,16 +5,23 @@ GameBoy gb;
 int paddleX = 3;
 int paddleY = 14;
 
+int paddleT = 0;
+int ballT = 0;
+
+int ballX = 4;
+int ballY = 13;
+int directionX = 1;
+int directionY = -1;
+
 void setup() {
   gb.begin(8);
+  paddleT = millis();
+  ballT = millis();
 }
 
 void loop() {
+  ball();
   makePaddle();
-  gb.drawDisplay();
-  drawPaddle(paddle, paddleX, paddleY);
-  delay(200);
-
 }
 
 void drawPaddle(byte arr [3], int x, int y) {
@@ -26,10 +33,40 @@ void drawPaddle(byte arr [3], int x, int y) {
 }
 
 void makePaddle() {
-  if (gb.getKey() == 4 && paddleX > 0) {
-    paddleX--;
+  if (gb.getKey() > 0 && millis() - paddleT >= 50) {
+    gb.drawDisplay();
+    if (gb.getKey() == 4 && paddleX > 0) {
+      paddleX--;
+    }
+    if (gb.getKey() == 5 && paddleX < 5) {
+      paddleX++;
+    }
+    paddleT = millis();
+    drawPaddle(paddle, paddleX, paddleY);
   }
-  if (gb.getKey() == 5 && paddleX < 5) {
-    paddleX++;
+}
+
+void ball() {
+  if (millis() - ballT >= 100) {
+    gb.wipePoint(ballX, ballY);
+    ballX += directionX;
+    ballY += directionY;
+    checkCollision();
+    gb.drawPoint(ballX, ballY);
+    ballT = millis();
+  }
+}
+
+
+void checkCollision() {
+  if (ballX <= 0 || ballX >= 7) directionX = directionX * -1;
+  if (ballY <= 0 || ballY >= 15) directionY = directionY * -1;
+  if (ballY == paddleY - 1 && ballX >= paddleX && ballX <= paddleX + 3) {
+    directionY = -1;
+    if (random(0, 10) < 5) {
+      directionX = 1;
+    } else {
+      directionX = -1;
+    }
   }
 }
