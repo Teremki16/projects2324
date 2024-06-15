61f654a0ab49177947ca9c1d19985fa37a4393f5
Teremki16 <teremki16robocode@gmail.com>
2024-06-15 08:36:18 +0300

    Update at 15.06.2024

--- SB10 Middle embedded/liza_ARCANOID___/liza_ARCANOID___.ino
+++ SB10 Middle embedded/liza_ARCANOID___/liza_ARCANOID___.ino
@@ -5,8 +5,8 @@ GameBoy gb;
 int paddleX = 3;
 int paddleY = 14;
 
-int paddleT = 0;
-int ballT = 0;
+ unsigned long int paddleT = 0;
+ unsigned long int ballT = 0;
 
 int ballX = 4;
 int ballY = 13;
@@ -14,11 +14,17 @@ int ballY = 13;
 int directionX = 1;
 int directionY = -1;
 
+bool statelevel = true;
+int numLevel = 1;
+
 void setup() {
   gb.begin(8);
   paddleT = millis();
   ballT = millis();
-  drawPaddle(paddle, paddleX, paddleY);
+  drawPaddle(Paddle, paddleX, paddleY);
+  
+  createLevel();
+
 }
 
 void loop() {
@@ -70,11 +76,66 @@ void checkCollision() {
   if (ballY <= 0 || ballY >= 15) directionY = directionY * -1;
   if (ballY == paddleY - 1 && ballX >= paddleX && ballX <= paddleX + 3) {
     directionY = -1;
-    if (random(0, 10) < 5) {
+    int d = random(0, 10);
+    if ( d < 3) {
+    
       directionX = 1;
-    } else {
+    } else if( d < 6){
       directionX = -1;
+    }else{
+      directionX = 0;
 
     }
   }
+  if(gb.checkCollision(ballX, ballY)){
+    gb.wipePoint(ballX, ballY);
+    directionY = 1;
+  }
 }
+
+void drawBricks( byte arr[3][8]){
+  for(int i = 0; i < 4; i++){
+    for(int j = 0; j < 8; j++){
+      if(arr[i][j] == 1);
+      gb.memDisplay(j, i);
+    }
+  }
+}
+
+void memClear(){
+ for(int i = 0; i < 4; i++){
+    for(int j = 0; j < 8; j++){
+      gb.display[j][i] = 0;
+    }
+  }  
+}
+
+void createLevel(){
+  memClear();
+  if(numLevel == 1){
+    gb.clearDisplay();
+    drawBricks(Block_level_1);
+    statelevel = true;
+  }
+  if(numLevel == 2){
+    gb.clearDisplay();
+    drawBricks(Block_level_2);
+    statelevel = true;
+  }  
+  if(numLevel == 3){
+    gb.clearDisplay();
+    drawBricks(Block_level_3);
+    statelevel = true;
+  }  
+  if(numLevel == 4){
+    gb.clearDisplay();
+    drawBricks(Block_level_4);
+    statelevel = true;
+  }  
+  if(numLevel == 5){
+    gb.clearDisplay();
+    drawBricks(Block_level_5);
+    statelevel = true;
+  }  
+ }
+  
