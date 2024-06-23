dd08e66e32cf35eeeac5adb52acf25751584098e
Teremki16 <teremki16robocode@gmail.com>
2024-06-23 08:49:36 +0300

    Update at 23.06.2024

--- SB10 Middle embedded/arcanoid_Bohdanovych/arcanoid_Bohdanovych.ino
+++ SB10 Middle embedded/arcanoid_Bohdanovych/arcanoid_Bohdanovych.ino
@@ -9,22 +9,29 @@ int ballX = 4;
 int ballY = 13;
 int directionX = 1;
 int directionY = -1;
+bool stateLevel = true;
+int numLevel = 1;
 
-int paddleT = 0;
-int ballT = 0;
+unsigned long int paddleT = 0;
+unsigned long int ballT = 0;
+
+int count
 
 
 void setup() {
   gb.begin(8);
   paddleT = millis();
   ballT = millis();
-  drawPaddle(paddle, paddleX, paddleY);s
-  
-  
+  drawPaddle(paddle, paddleX, paddleY);
+
+  createLevel();
+
+
 
 }
 
 void loop() {
+  Alose();
   ball();
   makePaddle();
 }
@@ -68,12 +75,85 @@ void checkCollision() {
   if (ballY <= 0 || ballY >= 15) directionY = directionY * -1;
   if (ballY == paddleY - 1 && ballX >= paddleX && ballX <= paddleX + 3) {
     directionY = -1;
-    if (random(0, 10) < 5) {
+    int d = random(0, 10);
+    if (d < 3) {
       directionX = 1;
+    } else if (d < 6) {
+      directionX = -1;
     } else {
-      directionY = -1;
-
-
+      directionX = 0;
     }
   }
+  if (gb.checkCollision(ballX, ballY)) {
+    gb.wipePoint(ballX, ballY);
+    directionY = 1;
+  }
+}
+
+void drawBricks(byte arr[3][8]) {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 8; j++) {
+      if (arr[i][j] == 1)
+        gb.memDisplay(j, i);
+    }
+  }
+}
+
+void memClear() {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 8; j++) {
+      gb.display[j][i] = 0;
+    }
+  }
+}
+
+void createLevel() {
+  memClear();
+  if (numLevel == 1) {
+    gb.clearDisplay();
+    drawBricks(Block_level_1);
+    stateLevel = true;
+  }
+  if (numLevel == 2) {
+    gb.clearDisplay();
+    drawBricks(Block_leveL_2);
+    stateLevel = true;
+  }
+  if (numLevel == 3) {
+    gb.clearDisplay();
+    drawBricks(Block_leveL_3);
+    stateLevel = true;
+  }
+  if (numLevel == 4) {
+    gb.clearDisplay();
+    drawBricks(Block_leveL_4);
+    stateLevel = true;
+  }
+  if (numLevel == 5) {
+    gb.clearDisplay();
+    drawBricks(Block_leveL_5);
+    stateLevel = true;
+  }
+}
+
+void Alose() {
+  if (ballY >= 15) {
+    for (int i = 0; i < 16; i++) {
+      for (int j = 0; j < 8; j++) {
+        gb.wipePoint(j,i);
+      }
+    }
+    gb.testMatrix(10);
+    restart();
+  }
+}
+
+
+void restart(){
+  ballX = 3;
+  ballY = 8;
+  directionX = 1;
+  directionY = -1;
+  createLevel();
+  drawPaddle(paddle ,paddleX, paddleY);
 }
